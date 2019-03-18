ddaf2cac3730b1794c2d5b305292341cecc83c38
Daniel Elps <delps@ineteng.com>
2019-03-18 10:13:42 -0400

    Removed index property from UDR, this will be indicated by place in array.

--- src/app/models/user-defined-route.ts
+++ src/app/models/user-defined-route.ts
@@ -3,8 +3,6 @@ import { NetworkSecurityProfileRule } from './network-security-profile-rule';
 
 export class UserDefinedRoute {
 
-    Index: number;
-
     NextHop: string;
 
     Rules: Array<NetworkSecurityProfileRule>;
