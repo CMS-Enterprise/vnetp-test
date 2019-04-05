de6cb5da1ce0174ab2241a543bc8eeb424687ac5
Daniel Elps <delps@ineteng.com>
2019-04-05 16:46:35 -0400

    Continuing refactor of ip address service and create network component.

--- src/app/components/networks/create-network/create-network.component.ts
+++ src/app/components/networks/create-network/create-network.component.ts
@@ -1,3 +1,6 @@
+// TODO: Needs Cleanup
+// TODO: Needs further validation against existing subnets in D42
+
 import { Component, OnInit } from '@angular/core';
 import { AutomationApiService } from 'src/app/services/automation-api.service';
 import { Router } from '@angular/router';
@@ -11,46 +14,73 @@ import { IpAddressService } from 'src/app/services/ip-address.service';
   styleUrls: ['./create-network.component.css']
 })
 export class CreateNetworkComponent implements OnInit {
-
   network = new Network();
 
-  constructor(private automationApiService: AutomationApiService, private toastr: ToastrService, private router: Router,
-              private ipService: IpAddressService) { }
+  cidrAddress: string;
+  subnetMask: string;
+  usableRange: string;
+  rangeSize: string;
 
-  ngOnInit() {
-  }
+  constructor(
+    private automationApiService: AutomationApiService,
+    private toastr: ToastrService,
+    private router: Router,
+    private ipService: IpAddressService
+  ) {}
 
-  validateIp(ipAddress: string) {
-     return this.ipService.isIpAddress(ipAddress);
-  }
+  ngOnInit() {}
 
-  calculateSubnetMask() {
-    if (this.network.SubnetMaskBits < 0) { this.network.SubnetMaskBits = 0; }
-    if (this.network.SubnetMaskBits > 32 ) { this.network.SubnetMaskBits = 32; }
+  calculateNetwork() {
 
-    if (!this.validateIp(this.network.NetworkAddress)) { return; }
+    const [isValid, error] = this.ipService.isValidIPv4CidrNotation(this.cidrAddress);
+    if (!isValid) { return; }
 
-    var result =  this.ipService.calculateSubnetMask(this.network.NetworkAddress, this.network.SubnetMaskBits);
+    // If the range portion of the CIDR address is greater than 30, set it to 30.
+    if (!this.ipService.ipv4MaskLessThan(this.cidrAddress, 30)) {
+     this.cidrAddress = this.ipService.updateCidrMask(this.cidrAddress, 30);
+   }
 
-    this.network.SubnetMask = result.prefixMaskStr;
+    let ipv4Range = this.ipService.getIpv4Range(this.cidrAddress);
+    this.usableRange = `${ipv4Range.getFirst().nextIPNumber().nextIPNumber()}-${ipv4Range.getLast()}`;
+    this.rangeSize = ipv4Range.getSize().toString();
+
+    this.network.NetworkAddress = ipv4Range.getFirst().nextIPNumber().toString();
+    this.network.SubnetMask = this.ipService.calculateSubnetMask(this.cidrAddress);
+    this.network.SubnetMaskBits = this.ipService.getCidrMask(this.cidrAddress);
   }
 
   createNetwork() {
 
-    if (!this.network.Name || !this.network.NetworkAddress || !this.network.SubnetMaskBits || !this.network.VlanId) {
-        this.toastr.error('Invalid Data');
-        return;
-      }
+    const [isValid, error] = this.ipService.isValidIPv4CidrNotation(this.cidrAddress);
+
+    if (
+      !isValid ||
+      !this.network.Name ||
+      !this.network.NetworkAddress ||
+      !this.network.SubnetMaskBits ||
+      !this.network.VlanId
+    ) {
+      this.toastr.error('Invalid Data');
+      return;
+    }
 
     const body = {
-      extra_vars: `{\"vlan_id\": ${this.network.VlanId},\"ip_address\": ${this.network.NetworkAddress}
-      ,\"subnet_mask\": ${this.network.SubnetMask},\"customer_id\": ${this.network.Name}
+      extra_vars: `{\"vlan_id\": ${this.network.VlanId},\"ip_address\": ${
+        this.network.NetworkAddress
+      }
+      ,\"subnet_mask\": ${this.network.SubnetMask},\"customer_id\": ${
+        this.network.Name
+      }
       ,\"subnet_mask_bits\": ${this.network.SubnetMaskBits}}`
     };
 
-    this.automationApiService.launchTemplate('create_asa_subinterface', body).subscribe();
+    this.automationApiService
+      .launchTemplate('create_asa_subinterface', body)
+      .subscribe();
     this.automationApiService.launchTemplate('create_vlan', body).subscribe();
-    this.automationApiService.launchTemplate('create_device42_subnet', body).subscribe();
+    this.automationApiService
+      .launchTemplate('create_device42_subnet', body)
+      .subscribe();
     this.router.navigate(['/networks']);
   }
 }
--- src/app/services/ip-address.service.ts
+++ src/app/services/ip-address.service.ts
@@ -1,18 +1,64 @@
-import { Injectable } from '@angular/core';
-import * as IpSubnetCalculator from 'ip-subnet-calculator/lib/ip-subnet-calculator.js';
+import { Injectable } from "@angular/core";
+import { IPv4 } from "ip-num/IPv4";
+import { IPv6 } from "ip-num/IPv6";
+import { IPv4Range } from "ip-num/IPv4Range";
+import { Validator } from "ip-num/Validator";
+import * as IpSubnetCalculator from "ip-subnet-calculator/lib/ip-subnet-calculator.js";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class IpAddressService {
-
   constructor() {}
 
-   public isIpAddress(ipAddress: string) {
-     return IpSubnetCalculator.isIp(ipAddress);
-   }
+  public isValidIPv4String(ipAddress: string): [boolean, string[]] {
+    return Validator.isValidIPv4String(ipAddress);
+  }
 
-   public calculateSubnetMask(ipAddress: string, prefixSize: number): any {
-     return IpSubnetCalculator.calculateSubnetMask(ipAddress, prefixSize);
-   }
+  public isValidIPv4CidrNotation(cidr: string): [boolean, string[]] {
+    return Validator.isValidIPv4CidrNotation(cidr);
+  }
+
+  public getIpv4Range(cidr: string): IPv4Range {
+    return IPv4Range.fromCidr(cidr);
+  }
+
+  public ipv4MaskLessThan(cidr: string, length: number) {
+    let cidrComponents = cidr.split('/');
+
+    let ip = cidrComponents[0];
+    let range = cidrComponents[1];
+
+    if (+range > length) {
+      return false;
+    }
+    return true;
+  }
+
+  public updateCidrMask(cidr: string, value: number) {
+    let cidrComponents = cidr.split('/');
+
+    let ip = cidrComponents[0];
+    let range = cidrComponents[1];
+
+    return `${ip}/${value}`;
+  }
+
+  public calculateSubnetMask(cidr: string): string{
+    let cidrComponents = cidr.split('/');
+
+    let ip = cidrComponents[0];
+    let range = cidrComponents[1];
+
+    return IpSubnetCalculator.calculateSubnetMask(ip, range).prefixMaskStr;
+  }
+
+  public getCidrMask(cidr: string): number {
+    let cidrComponents = cidr.split('/');
+
+    let ip = cidrComponents[0];
+    let range = cidrComponents[1];
+
+    return +range;
+  }
 }
