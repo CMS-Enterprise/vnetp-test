8f7afa50dae063099f8f30e38e3a2c12060ae204
Daniel Elps <delps@ineteng.com>
2019-05-06 13:14:50 -0400

    Formatting fix.

--- src/app/modals/firewall-rule-modal/firewall-rule-modal.component.ts
+++ src/app/modals/firewall-rule-modal/firewall-rule-modal.component.ts
@@ -18,10 +18,8 @@ export class FirewallRuleModalComponent implements OnInit {
   destinationNetworkTypeSubscription: Subscription;
   destinationServiceTypeSubscription: Subscription;
 
-  constructor(
-    private ngx: NgxSmartModalService,
-    private formBuilder: FormBuilder
-  ) {}
+  constructor(private ngx: NgxSmartModalService, private formBuilder: FormBuilder) {
+  }
 
   save() {
     this.submitted = true;
@@ -44,60 +42,40 @@ export class FirewallRuleModalComponent implements OnInit {
     this.reset();
   }
 
-  get f() {
-    return this.form.controls;
-  }
+  get f() { return this.form.controls; }
 
   ngOnInit() {
     this.buildForm();
   }
 
   getData() {
-    const firewallRule = Object.assign({}, this.ngx.getModalData(
-      'firewallRuleModal'
-    ) as NetworkSecurityProfileRule);
+    const firewallRule = Object.assign({}, this.ngx.getModalData('firewallRuleModal') as NetworkSecurityProfileRule);
     if (firewallRule !== undefined) {
+
       this.form.controls.name.setValue(firewallRule.Name);
       this.form.controls.action.setValue(firewallRule.Action);
       this.form.controls.protocol.setValue(firewallRule.Protocol);
       this.form.controls.direction.setValue(firewallRule.Direction);
 
       this.form.controls.sourceIp.setValue(firewallRule.SourceIP);
-      this.form.controls.sourceNetworkObject.setValue(
-        firewallRule.SourceNetworkObject
-      );
-      this.form.controls.sourceNetworkObjectGroup.setValue(
-        firewallRule.SourceNetworkObjectGroup
-      );
+      this.form.controls.sourceNetworkObject.setValue(firewallRule.SourceNetworkObject);
+      this.form.controls.sourceNetworkObjectGroup.setValue(firewallRule.SourceNetworkObjectGroup);
 
       this.form.controls.sourcePorts.setValue(firewallRule.SourcePorts);
-      this.form.controls.sourceServiceObject.setValue(
-        firewallRule.SourceServiceObject
-      );
-      this.form.controls.sourceServiceObjectGroup.setValue(
-        firewallRule.SourceServiceObjectGroup
-      );
+      this.form.controls.sourceServiceObject.setValue(firewallRule.SourceServiceObject);
+      this.form.controls.sourceServiceObjectGroup.setValue(firewallRule.SourceServiceObjectGroup);
 
       this.form.controls.destinationIp.setValue(firewallRule.DestinationIP);
-      this.form.controls.destinationNetworkObject.setValue(
-        firewallRule.DestinationNetworkObject
-      );
-      this.form.controls.destinationNetworkObjectGroup.setValue(
-        firewallRule.DestinationNetworkObjectGroup
-      );
+      this.form.controls.destinationNetworkObject.setValue(firewallRule.DestinationNetworkObject);
+      this.form.controls.destinationNetworkObjectGroup.setValue(firewallRule.DestinationNetworkObjectGroup);
 
-      this.form.controls.destinationPorts.setValue(
-        firewallRule.DestinationPorts
-      );
-      this.form.controls.destinationServiceObject.setValue(
-        firewallRule.DestinationServiceObject
-      );
-      this.form.controls.destinationServiceObjectGroup.setValue(
-        firewallRule.DestinationServiceObjectGroup
-      );
+      this.form.controls.destinationPorts.setValue(firewallRule.DestinationPorts);
+      this.form.controls.destinationServiceObject.setValue(firewallRule.DestinationServiceObject);
+      this.form.controls.destinationServiceObjectGroup.setValue(firewallRule.DestinationServiceObjectGroup);
 
       this.form.controls.log.setValue(firewallRule.Log);
-    }
+
+      }
   }
 
   private buildForm() {
@@ -106,7 +84,7 @@ export class FirewallRuleModalComponent implements OnInit {
       description: [''],
       action: ['', Validators.required],
       protocol: ['', Validators.required], // TODO: Only required when using source ports, otherwise inherited from
-      // service object/service object group.
+                                           // service object/service object group.
       direction: [''],
 
       // Source Network Info
@@ -137,20 +115,17 @@ export class FirewallRuleModalComponent implements OnInit {
     });
   }
 
-  private unsubAll() {
+  private unsubAll(){
     try {
-      [
-        this.sourceNetworkTypeSubscription,
-        this.sourceServiceTypeSubscription,
-        this.destinationNetworkTypeSubscription,
-        this.destinationServiceTypeSubscription
-      ].forEach(sub => {
-        if (sub) {
-          sub.unsubscribe();
-        }
-      });
+      [ this.sourceNetworkTypeSubscription, this.sourceServiceTypeSubscription,
+        this.destinationNetworkTypeSubscription, this.destinationServiceTypeSubscription]
+        .forEach( sub => {
+          if (sub) {
+            sub.unsubscribe();
+          }
+        });
     } catch (e) {
-      console.error(e);
+       console.error(e);
     }
   }
 
