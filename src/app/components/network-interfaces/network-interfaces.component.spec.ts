f4e98709b0408d2697e698fb6ccb0817ad63992a
Daniel Elps <delps@ineteng.com>
2019-05-25 12:25:15 -0400

    Add minimal test coverage for net and log int forms.

--- src/app/components/network-interfaces/network-interfaces.component.spec.ts
+++ src/app/components/network-interfaces/network-interfaces.component.spec.ts
@@ -1,6 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NetworkInterfacesComponent } from './network-interfaces.component';
+import { LogicalInterfaceModalComponent } from 'src/app/modals/logical-interface-modal/logical-interface-modal.component';
+import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { NgxSmartModalModule, NgxSmartModalService } from 'ngx-smart-modal';
+import { PapaParseModule } from 'ngx-papaparse';
+import { ToastrModule } from 'ngx-toastr';
+import { NgxMaskModule } from 'ngx-mask';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { HttpClientModule, HttpHandler, HttpClient } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
 describe('NetworkInterfacesComponent', () => {
   let component: NetworkInterfacesComponent;
@@ -8,7 +17,27 @@ describe('NetworkInterfacesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NetworkInterfacesComponent ]
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        NgxSmartModalModule,
+        PapaParseModule,
+        ToastrModule.forRoot(),
+        NgxMaskModule.forRoot(),
+        AngularFontAwesomeModule
+      ],
+      declarations: [
+NetworkInterfacesComponent,
+LogicalInterfaceModalComponent
+      ],
+      providers: [
+        NgxSmartModalService,
+        HttpClientModule,
+        HttpClient,
+        HttpHandler,
+        CookieService,
+        FormBuilder
+      ]
     })
     .compileComponents();
   }));
--- src/app/modals/logical-interface-modal/logical-interface-modal.component.spec.ts
+++ src/app/modals/logical-interface-modal/logical-interface-modal.component.spec.ts
@@ -1,46 +1,59 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { NgxSmartModalService, NgxSmartModalModule } from 'ngx-smart-modal';
-// import { FormsModule, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
-// import { NgxMaskModule } from 'ngx-mask';
-// import { PoolModalComponent } from './logical-interface-modal.component';
-// import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgxSmartModalService, NgxSmartModalModule } from 'ngx-smart-modal';
+import { FormsModule, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { LogicalInterfaceModalComponent } from './logical-interface-modal.component';
 
-// describe('PoolModalComponent', () => {
-//   let component: PoolModalComponent;
-//   let fixture: ComponentFixture<PoolModalComponent>;
+describe('LogicalInterfaceModalComponent', () => {
+  let component: LogicalInterfaceModalComponent;
+  let fixture: ComponentFixture<LogicalInterfaceModalComponent>;
 
-//   const ngx: NgxSmartModalService = new NgxSmartModalService();
+  const ngx: NgxSmartModalService = new NgxSmartModalService();
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       imports: [ FormsModule, NgxSmartModalModule, ReactiveFormsModule, AngularFontAwesomeModule,NgxMaskModule.forRoot()],
-//       declarations: [ PoolModalComponent ],
-//       providers: [ { provide: NgxSmartModalService, useValue: ngx }, FormBuilder, Validators]
-//     })
-//     .compileComponents().then(() => {
-//       fixture = TestBed.createComponent(PoolModalComponent);
-//       component = fixture.componentInstance;
-//     });
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [ FormsModule, NgxSmartModalModule, ReactiveFormsModule, AngularFontAwesomeModule,NgxMaskModule.forRoot()],
+      declarations: [ LogicalInterfaceModalComponent ],
+      providers: [ { provide: NgxSmartModalService, useValue: ngx }, FormBuilder, Validators]
+    })
+    .compileComponents().then(() => {
+      fixture = TestBed.createComponent(LogicalInterfaceModalComponent);
+      component = fixture.componentInstance;
+    });
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(PoolModalComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogicalInterfaceModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   // Initial Form State
-//   it('name should be required', () => {
-//     const name = component.form.controls.name;
-//     expect(name.valid).toBeFalsy();
-//   });
+  it('user should not be able to add native vlan as tagged vlan', () => {
+    const nativeSubnet = component.form.controls.nativeSubnet;
+    nativeSubnet.setValue('Test1');
 
-//   it('load balancing should be required', () => {
-//     const loadBalancingMethod = component.form.controls.loadBalancingMethod;
-//     expect(loadBalancingMethod.valid).toBeFalsy();
-//   });
-// });
+    const selectedTaggedSubnet = component.form.controls.selectedTaggedSubnet;
+    selectedTaggedSubnet.setValue('Test1');
+    component.selectSubnet();
+
+    expect(component.selectedSubnets.length).toBeFalsy();
+  })
+
+  // Initial Form State
+  it('name should be required', () => {
+    const name = component.form.controls.name;
+    expect(name.valid).toBeFalsy();
+  });
+
+  it('native vlan should be required', () => {
+    const nativeSubnet = component.form.controls.nativeSubnet;
+    expect(nativeSubnet.valid).toBeFalsy();
+  });
+
+
+});
