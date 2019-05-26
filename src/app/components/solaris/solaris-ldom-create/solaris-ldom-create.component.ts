d1ab27b9192012c32b401e3376ecccf66bd4aa7a
Nick Atkins <natkins@ineteng.com>
2019-05-26 10:32:48 -0400

    Fixing formatting issues

--- src/app/components/solaris/solaris-ldom-create/solaris-ldom-create.component.ts
+++ src/app/components/solaris/solaris-ldom-create/solaris-ldom-create.component.ts
@@ -32,44 +32,44 @@ export class SolarisLdomCreateComponent implements OnInit {
     private router: Router,
     private messageService: MessageService,
     private authService: AuthService
-    ) { 
+    ) {
     this.vnets = new Array<any>();
     this.LDOM = new SolarisLdom();
 
   }
-  printCDOM(cdomInput: SolarisCdom){
+  printCDOM(cdomInput: SolarisCdom) {
     this.currentCDOM = cdomInput;
   }
-  addvnetObject(obj: any, objArray: Array<any>){
+  addvnetObject(obj: any, objArray: Array<any>) {
      objArray.push(obj);
      this.inputLDOMvnet = '';
   }
-  addvdsObject(objVDSwwn: string, objvDiskname: string, objVDS: string, objArray: Array<any>){
+  addvdsObject(objVDSwwn: string, objvDiskname: string, objVDS: string, objArray: Array<any>) {
     objArray.push(`${objVDSwwn},${objvDiskname},${objVDS}`);
     this.inputLDOMvds = '';
     this.inputLDOMvdswwn = '';
     this.inputLDOMvdiskname = '';
  }
-  getvnetCmds(){
-   // Create commands that will be sent as add-vnet parameter 
-   for(let i = 0; i < this.LDOM.add_vnet.length;i++){
-    const vnetCmdString = `id=${i} vnet${i} ${this.LDOM.add_vnet[i]}`
+  getvnetCmds() {
+   // Create commands that will be sent as add-vnet parameter
+   for (let i = 0; i < this.LDOM.add_vnet.length; i++) {
+    const vnetCmdString = `id=${i} vnet${i} ${this.LDOM.add_vnet[i]}`;
     this.LDOM.add_vnet_cmd.push(vnetCmdString);
     this.inputLDOMvnet = '';
    }
    console.log(this.LDOM.add_vnet_cmd);
   }
-  getvdsCmds(){
-    // Create commands that will be sent as add-vnet parameter 
-    for(let i = 0; i < this.LDOM.add_vds.length;i++){
+  getvdsCmds() {
+    // Create commands that will be sent as add-vnet parameter
+    for (let i = 0; i < this.LDOM.add_vds.length; i++) {
      const WWN = this.LDOM.add_vcpu[i].split(',')[0];
      const diskName = this.LDOM.add_vcpu[i].split(',')[1];
      const VDS = this.LDOM.add_vcpu[i].split(',')[2];
-     const vdsCmdString = `/dev/disk/c0t${WWN}d0s2 ${diskName}@${VDS}`
-     const vdiskCmdString = `id=${i} ${diskName} ${diskName}@${VDS}`
+     const vdsCmdString = `/dev/disk/c0t${WWN}d0s2 ${diskName}@${VDS}`;
+     const vdiskCmdString = `id=${i} ${diskName} ${diskName}@${VDS}`;
      this.LDOM.add_vds_cmd.push(vdsCmdString);
      this.LDOM.add_vdisk_cmd.push(vdiskCmdString);
-     
+
      // add_vdisk: 'id=0 lde43s032pdisk1 lde43s032pdisk1@primary-vds0'
     }
     this.inputLDOMvdiskname = '';
@@ -78,31 +78,32 @@ export class SolarisLdomCreateComponent implements OnInit {
     console.log(this.LDOM.add_vds_cmd);
     console.log(this.LDOM.add_vdisk_cmd);
    }
-  addvds(vdsWWN: string){
+  addvds(vdsWWN: string) {
    this.LDOM.add_vds.push(this.inputLDOMvds);
-   //Create commands that will be sent as add-vnet parameter 
-   const vdsIndex = this.LDOM.add_vds.length - 1
-   const vdsCmdString = `/dev/dsk/${vdsWWN} ${this.LDOM.name}@${this.inputLDOMvds}`
+   // Create commands that will be sent as add-vnet parameter
+   const vdsIndex = this.LDOM.add_vds.length - 1;
+   const vdsCmdString = `/dev/dsk/${vdsWWN} ${this.LDOM.name}@${this.inputLDOMvds}`;
    this.LDOM.add_vds_cmd.push(vdsCmdString);
    this.inputLDOMvds = '';
   }
-  moveObjectPosition(value: number, obj, objArray){
-   this.solarisService.moveObjectPosition(value, obj, objArray)
+  moveObjectPosition(value: number, obj, objArray) {
+   this.solarisService.moveObjectPosition(value, obj, objArray);
   }
-  deleteObject(obj, objArray){
+  deleteObject(obj, objArray) {
     this.solarisService.deleteObject(obj, objArray);
   }
   launchLDOMJobs() {
-    let extra_vars: {[k: string]: any} = {};
+  // tslint:disable-next-line: variable-name
+    const extra_vars: {[k: string]: any} = {};
     this.LDOM.customer_name = this.authService.currentUserValue.CustomerName;
-    this.LDOM.devicetype = "solaris_ldom";
+    this.LDOM.devicetype = 'solaris_ldom';
     extra_vars.LDOM = this.LDOM;
 
     const body = { extra_vars };
 
-    //this.automationApiService
-    //const customerName = this.solarisService.getCustomerNamebyDeviceID(this.LDOM.device_id);
-    const customerName = "contoso";
+    // this.automationApiService
+    // const customerName = this.solarisService.getCustomerNamebyDeviceID(this.LDOM.device_id);
+    const customerName = 'contoso';
     this.automationApiService.launchTemplate(`${customerName}-save-device`, body).subscribe();
     this.messageService.filter('Job Launched');
     this.router.navigate(['/solaris']);
@@ -113,16 +114,15 @@ export class SolarisLdomCreateComponent implements OnInit {
     this.getLdoms();
     */
    this.automationApiService.doqlQuery(
-    "SELECT * FROM view_device_custom_fields_flat_v1 cust LEFT JOIN view_device_v1 std ON std.device_pk = cust.device_fk"
+    'SELECT * FROM view_device_custom_fields_flat_v1 cust LEFT JOIN view_device_v1 std ON std.device_pk = cust.device_fk'
   )
   .subscribe(data => {
 
     this.returnDevices = this.solarisService.loadDevices(data);
     this.returnDevices.forEach((obj) => {
-     if(obj.key === "LDOM"){
+     if (obj.key === 'LDOM') {
        this.LDOMDeviceArray = obj.value;
-     }
-     else if(obj.key === "CDOM"){
+     } else if (obj.key === 'CDOM') {
       this.CDOMDeviceArray = obj.value;
      }
      // this.vnets = this.CDOMDeviceArray
