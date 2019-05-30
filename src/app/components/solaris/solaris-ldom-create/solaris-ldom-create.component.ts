7b7a6e1369d51e02cf3ad7982d5231fe216bef40
Daniel Elps <delps@ineteng.com>
2019-05-30 09:02:47 -0400

    Remove invalid import.

--- src/app/components/solaris/solaris-ldom-create/solaris-ldom-create.component.ts
+++ src/app/components/solaris/solaris-ldom-create/solaris-ldom-create.component.ts
@@ -1,13 +1,11 @@
-import { Component, OnInit, KeyValueDiffers } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SolarisService } from '../solaris-services/solaris-service.service';
 import { SolarisLdom, SolarisVariable } from 'src/app/models/solaris-ldom';
 import { AutomationApiService } from 'src/app/services/automation-api.service';
 import { Router } from '@angular/router';
 import { MessageService } from 'src/app/services/message.service';
-import { extractStyleParams } from '@angular/animations/browser/src/util';
 import { SolarisCdom, SolarisCdomResponse } from 'src/app/models/solaris-cdom';
 import { AuthService } from 'src/app/services/auth.service';
-import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-solaris-ldom-create',
