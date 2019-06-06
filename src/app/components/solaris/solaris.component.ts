b02604a910b41160300c2633d1f98d043a15d18b
Daniel Elps <delps@ineteng.com>
2019-06-06 08:32:20 -0400

    Solaris dash shows real numbers, other minor fixes.

--- src/app/components/solaris/solaris.component.ts
+++ src/app/components/solaris/solaris.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AutomationApiService } from 'src/app/services/automation-api.service';
 
 @Component({
   selector: 'app-solaris',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolarisComponent implements OnInit {
 
-  constructor() { }
+  constructor(private automationApiService: AutomationApiService) { }
+
+  CdomCount: number;
+  LdomCount: number;
 
   ngOnInit() {
+    this.getCdomCount();
+    this.getLdomCount();
   }
 
+  getCdomCount() {
+    this.automationApiService.getCDoms().subscribe(data => {
+      const result = data as any;
+      this.CdomCount = result.total_count;
+    });
+  }
+
+  getLdomCount() {
+    this.automationApiService.getLDoms().subscribe(data => {
+      const result = data as any;
+      this.LdomCount = result.total_count;
+    });
+  }
 }
