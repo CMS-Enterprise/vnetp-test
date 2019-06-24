31bdaafd00eed6b8151fa4af41e1dfc8b9c6c64b
Mariel Van Norman <mariel.van.norman@levvel.io>
2019-06-24 13:48:06 -0400

    wip design updates

--- src/app/components/breadcrumb/breadcrumb.component.ts
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -6,7 +6,8 @@ import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-breadcrumb',
-  templateUrl: './breadcrumb.component.html'
+  templateUrl: './breadcrumb.component.html',
+  styleUrls: ['./breadcrumb.component.scss']
 })
 
 export class BreadcrumbComponent implements OnInit {
