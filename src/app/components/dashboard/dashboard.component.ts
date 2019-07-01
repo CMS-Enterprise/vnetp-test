8563a6d4c3a5b523493ff16c89e6767e42864953
Daniel Elps <delps@ineteng.com>
2019-07-01 13:11:52 -0400

    TSlint.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,106 +1,59 @@
-import { Component, OnInit } from "@angular/core"
-import { AutomationApiService } from "src/app/services/automation-api.service"
+import { Component, OnInit } from '@angular/core';
+import { AutomationApiService } from 'src/app/services/automation-api.service';
 
 @Component({
-  selector: "app-dashboard",
-  templateUrl: "./dashboard.component.html",
-  styleUrls: ["./dashboard.component.scss"],
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
   constructor(private automationApiService: AutomationApiService) {
-    this.ips = { ips: [] }
-    this.subnets = { subnets: [] }
-    this.devices = { Devices: [] }
-    this.jobs = { results: [] }
+    this.ips = { ips: [] };
+    this.subnets = { subnets: [] };
+    this.devices = { Devices: [] };
+    this.jobs = { results: [] };
   }
 
-  ips: any
-  subnets: any
-  devices: any
-  jobs: any
-  failedJobs = 0
-  successfulJobs = 0
+  ips: any;
+  subnets: any;
+  devices: any;
+  jobs: any;
+  failedJobs = 0;
+  successfulJobs = 0;
+  status: any;
 
-  dashboardPoller = setInterval(() => this.loadDashboard(), 1000 * 300)
-
-  // TODO: switch mock data for services endpoint
-  services: Array<Object> = [
-    {
-      title: "Data",
-      status: "green",
-    },
-    {
-      title: "Mainframe",
-      status: "green",
-    },
-    {
-      title: "Z/Linux",
-      status: "green",
-    },
-    {
-      title: "VMware",
-      status: "green",
-    },
-    {
-      title: "AWS",
-      status: "green",
-    },
-    {
-      title: "VMware Cloud",
-      status: "green",
-    },
-    {
-      title: "Infrastructure",
-      status: "green",
-    },
-    {
-      title: "Network",
-      status: "green",
-    },
-    {
-      title: "Shard Servies",
-      status: "green",
-    },
-    {
-      title: "User Portal",
-      status: "yellow",
-    },
-    {
-      title: "Automation",
-      status: "red",
-    },
-  ]
+  dashboardPoller = setInterval(() => this.loadDashboard(), 1000 * 300);
 
   ngOnInit() {
-    this.loadDashboard()
+    this.loadDashboard();
   }
 
   loadDashboard() {
-    this.getDevices()
-    this.getNetworks()
-    this.getIps()
+    this.getDevices();
+    this.getNetworks();
+    this.getIps();
     this.getStatus();
-    this.getJobs()
+    this.getJobs();
   }
 
   getNetworks() {
     this.automationApiService
       .getSubnets()
-      .subscribe(data => (this.subnets = data))
+      .subscribe(data => (this.subnets = data));
   }
 
   getIps() {
-    this.automationApiService.getIps().subscribe(data => (this.ips = data))
+    this.automationApiService.getIps().subscribe(data => (this.ips = data));
   }
 
   getDevices() {
     this.automationApiService
       .getDevices()
-      .subscribe(data => (this.devices = data))
+      .subscribe(data => (this.devices = data));
   }
 
   getJobs() {
-    const date = new Date().toISOString().slice(0, 10)
+    const date = new Date().toISOString().slice(0, 10);
 
     // TODO: Refactor this to provide a more accurate count
     // by showing failed jobs in the last day based on local
@@ -109,20 +62,18 @@ export class DashboardComponent implements OnInit {
       .getJobs(
         `?created__gte=${date}T00:00&created__lte=${date}T23:59&page_size=50`
       )
-      .subscribe(data => (this.jobs = data), error => {}, () => this.sortJobs())
+      .subscribe(data => (this.jobs = data), error => {}, () => this.sortJobs());
   }
 
   sortJobs() {
     this.successfulJobs = this.jobs.results.filter(
-      job => !job.failed && job.status === "successful"
-    ).length
-    this.failedJobs = this.jobs.results.filter(job => job.failed).length
+      job => !job.failed && job.status === 'successful'
+    ).length;
+    this.failedJobs = this.jobs.results.filter(job => job.failed).length;
   }
 
-  status: any;
-
   getStatus() {
-    this.automationApiService.getSystemStatus().subscribe(data =>{
+    this.automationApiService.getSystemStatus().subscribe(data => {
       this.status = data;
     });
   }
