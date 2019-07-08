a71c35ea329d55753adf5112c8f1fc7d6f5f0dca
Nick Atkins <natkins@ineteng.com>
2019-07-08 16:44:57 -0400

    Fixed variable name, added semicolons

--- src/app/services/help-text.ts
+++ src/app/services/help-text.ts
@@ -6,11 +6,11 @@ import { environment } from 'src/environments/environment';
 export class HelpText {
     wikiBase = environment.wikiBase;
     //Networking tool tip messages
-    networkGroupText = `Group of network objects (IPs, DNS names, etc..).  More info: <a href="${this.wikiBase}ipam#NetworkGroups">wiki</a>`
-    networkPortGroupText = `Group of ports. More info: <a href="${wikiBase}/IPAM#NetworkGroups">wiki</a>` 
+    networkGroupText = `Group of network objects (IPs, DNS names, etc..).  More info: <a href="${this.wikiBase}ipam#NetworkGroups">wiki</a>`;
+    networkPortGroupText = `Group of ports. More info: <a href="${this.wikiBase}/IPAM#NetworkGroups">wiki</a>`; 
     // Solaris tool tip messages
     solarisCdomCloneText = `Clone resources from previously configured host.  More info: <a href="${this.wikiBase}/solaris#CDOM_Create">Wiki</a>`;
-    solarisLdomVnicText = `Required CDOM to inherit vSwitch. More info: <a href="${wikiBase}/solaris#LDOM_Create">wiki</a>`
-    solarisLdomVariableText = `Used to pass in variables to the LDOM automation scripts. More info: <a href="${wikiBase}/solaris#LDOM_Create>Wiki</a>`
-    solarisLdomNetInstallText = `Used to choose a image to apply to boot disk.  More info: <a href="${wikiBase}/solaris#LDOM_Create">wiki</a>`
+    solarisLdomVnicText = `Required CDOM to inherit vSwitch. More info: <a href="${this.wikiBase}/solaris#LDOM_Create">wiki</a>`;
+    solarisLdomVariableText = `Used to pass in variables to the LDOM automation scripts. More info: <a href="${this.wikiBase}/solaris#LDOM_Create>Wiki</a>`;
+    solarisLdomNetInstallText = `Used to choose a image to apply to boot disk.  More info: <a href="${this.wikiBase}/solaris#LDOM_Create">wiki</a>`;
 }
