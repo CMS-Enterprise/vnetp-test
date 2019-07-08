4bb01b400a5ff207a5184c6831551447c809181a
Nick Atkins <natkins@ineteng.com>
2019-07-08 13:33:49 -0400

    Added base url to hostname in wikiBase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiBase: 'https://draas.cds-cloud-labs.net',
-  wikiBase: 'http://wiki.draas.cdsvdc.lcl'
+  wikiBase: 'http://wiki.draas.cdsvdc.lcl/index.php/UI'
 };
