8b8144e23414f772fbdfe05a757abd58fd402c61
Nick Atkins <natkins@ineteng.com>
2019-07-08 16:15:52 -0400

    Adding more tooltip text. Adding spacing between text and tooltip

--- src/app/services/help-text.ts
+++ src/app/services/help-text.ts
@@ -9,7 +9,7 @@ export class HelpText {
     networkGroupText = `Group of network objects (IPs, DNS names, etc..).  More info: ${this.wikiBase}ipam#NetworkGroups`
     
     // Solaris tool tip messages
-    solarisCdomCloneText = `Clone resources from previously configured host.  More info: ${this.wikiBase}/solaris#CDOM_Create`;
+    solarisCdomCloneText = `Clone resources from previously configured host.  More info: <a href="${this.wikiBase}/solaris#CDOM_Create">wiki</a>`;
 
 
 
