4ac203340727803c27e9732d01d5cf3ad36170cd
Daniel Elps <delps@ineteng.com>
2019-07-11 14:27:24 -0400

    Revert back to using env vars for wikibase.

--- src/app/services/help-text.ts
+++ src/app/services/help-text.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
 @Injectable({
     providedIn: 'root'
   })
 export class HelpText {
-    wikiBase = 'http://wiki.draas.cdsvdc.lcl/index.php/UI';
+    wikiBase = environment.wikiBase;
     //Networking tool tip messages
     networkGroupText = `Group of network objects (IPs, DNS names, etc..).  More info: <a href="${this.wikiBase}/ipam#Network_Groups">wiki</a>`;
     networkPortGroupText = `Group of ports. More info: <a href="${this.wikiBase}/ipam#Service_Groups">wiki</a>`;
