a90430d7352d2093e937ab39cbf65e2d971f213b
Daniel Elps <delps@ineteng.com>
2019-07-11 13:49:01 -0400

    Set wiki-base in help text class.

--- src/app/services/help-text.ts
+++ src/app/services/help-text.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 @Injectable({
     providedIn: 'root'
   })
 export class HelpText {
-    wikiBase = environment.wikiBase;
+    wikiBase = 'http://wiki.draas.cdsvdc.lcl/index.php/UI';
     //Networking tool tip messages
     networkGroupText = `Group of network objects (IPs, DNS names, etc..).  More info: <a href="${this.wikiBase}/ipam#Network_Groups">wiki</a>`;
     networkPortGroupText = `Group of ports. More info: <a href="${this.wikiBase}/ipam#Service_Groups">wiki</a>`;
