64eb76895bdeb0152e6f1c149f791b3f11ef2a8d
Daniel Elps <delps@ineteng.com>
2019-07-19 14:22:01 -0400

    Adding text for fw rule modal.

--- src/app/services/help-text-networking.ts
+++ src/app/services/help-text-networking.ts
@@ -1,3 +1,5 @@
+// tslint:disable: max-line-length
+
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
@@ -6,10 +8,21 @@ import { environment } from 'src/environments/environment';
 })
 export class HelpTextNetworking {
     wikiBase = environment.wikiBase;
-    //Networking tool tip messages
-    networkGroupText = `Group of network objects (IPs, DNS names, etc..).  More info: <a href="${this.wikiBase}/ipam#Network_Groups">wiki</a>`;
+
+
+
+    fwModalSourceNetworkType = `Type of Source Network (IP, Object, Object Group). More info: <a href="${this.wikiBase}/ipam#Network_Groups">wiki</a>`;
+    fwModalSourceServiceType = `Type of Source Service (Port/Port Range, Object, Object Group). More info: <a href="${this.wikiBase}/ipam#Network_Groups">wiki</a>`;
+
+    fwModalDestinationNetworkType = `Type of Destination Network (IP, Object, Object Group). More info: <a href="${this.wikiBase}/ipam#Network_Groups">wiki</a>`;
+    fwModalDestinationServiceType = `Type of Destination Service (Port/Port Range, Object, Object Group). More info: <a href="${this.wikiBase}/ipam#Network_Groups">wiki</a>`;
+
+
+
+    // Networking tool tip messages
+
     networkPortGroupText = `Group of ports. More info: <a href="${this.wikiBase}/ipam#Service_Groups">wiki</a>`;
 
-      //Load balancer tool tip messages
+      // Load balancer tool tip messages
       loadBalancerIRuleText = `F5 LoadBalancer language to create rich rules.  More info: <a href=${this.wikiBase}/load-balancer#iRules">wiki</a>`;
 }
