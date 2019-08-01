73c88e4f91edef80d4dd08d4fee79acb9a4ff52c
Daniel Elps <delps@ineteng.com>
2019-08-01 15:13:21 -0400

    Finishing up firewall rule modal tooltips.

--- src/app/services/help-text-networking.ts
+++ src/app/services/help-text-networking.ts
@@ -9,6 +9,13 @@ import { environment } from 'src/environments/environment';
 export class HelpTextNetworking {
     wikiBase = environment.wikiBase;
 
+    // Firewall Rule Modal
+
+    fwModalAction = `Action that the firewall will take on traffic that matches this rule.`;
+    fwModalDirection = `Direction that this traffic flow will take. 'In' represents traffic entering the VRF from external/intervrf and 'Out' represents traffic leaving the VRF to external/intervrf.`;
+    fwModalProtocol = `Layer 4 Protocol that matching traffic uses.`;
+    fwModalLog = `Indicates whether traffic that matches this rule should be logged.`;
+
     fwModalSourceNetworkType = `Type of Source Network (IP, Object, Object Group). More info: <a href="${this.wikiBase}/ipam#Network_Groups">wiki</a>`;
     fwModalSourceServiceType = `Type of Source Service (Port/Port Range, Object, Object Group). More info: <a href="${this.wikiBase}/ipam#Network_Groups">wiki</a>`;
 
@@ -20,12 +27,10 @@ export class HelpTextNetworking {
     fwModalNetworkObjectGroupType = `Network Object Group created under IPAM.`;
 
     fwModalPortServiceType = `Single port (80), Range of ports (22-23) or 'any' to match any Port.`;
-    fwModalServiceObjectType = `Service Object created under IPAM.`;
+    fwModalServiceObjectType = `Service Object created under IPAM. <a href="${this.wikiBase}/ipam#Service_Groups">wiki</a>`;
     fwModalServiceObjectGroupType = `Service Object Group created under IPAM.`;
 
-    // Networking tool tip messages
-    networkPortGroupText = `Group of ports. More info: <a href="${this.wikiBase}/ipam#Service_Groups">wiki</a>`;
 
-      // Load balancer tool tip messages
-      loadBalancerIRuleText = `F5 LoadBalancer language to create rich rules.  More info: <a href=${this.wikiBase}/load-balancer#iRules">wiki</a>`;
+    // Load balancer tool tip messages  
+    loadBalancerIRuleText = `F5 LoadBalancer language to create rich rules.  More info: <a href=${this.wikiBase}/load-balancer#iRules">wiki</a>`;
 }
