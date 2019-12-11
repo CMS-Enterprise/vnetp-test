a44d45478a29dd8624f0e370a7bfe17b29643479
delps@ineteng.com <delps@ineteng.com>
2019-12-11 13:06:04 -0500

    Updating network obj/grp component.

--- src/app/components/network-objects-groups/network-objects-groups.component.ts
+++ src/app/components/network-objects-groups/network-objects-groups.component.ts
@@ -29,8 +29,7 @@ export class NetworkObjectsGroupsComponent
 
   networkObjects: Array<NetworkObject>;
   networkObjectGroups: Array<NetworkObjectGroup>;
-  deletedNetworkObjects: Array<NetworkObject>;
-  deletedNetworkObjectGroups: Array<NetworkObjectGroup>;
+
   navIndex = 0;
 
   editNetworkObjectIndex: number;
@@ -64,6 +63,7 @@ export class NetworkObjectsGroupsComponent
   }
 
   getNetworkObjects() {
+    this.networkObjects = [];
     this.tierService
       .tiersIdGet(this.currentVrf.id, undefined, 'networkObjects')
       .subscribe(data => {
@@ -129,9 +129,10 @@ export class NetworkObjectsGroupsComponent
       .onAnyCloseEvent.subscribe((modal: NgxSmartModalComponent) => {
         const data = modal.getData() as NetworkObjectModalDto;
 
-        if (data && data.NetworkObject) {
-          this.saveNetworkObject(data.NetworkObject);
+        if (data) {
+          this.getNetworkObjects();
         }
+
         this.ngx.resetModalData('networkObjectModal');
         this.datacenterService.unlockDatacenter();
       });
@@ -144,86 +145,26 @@ export class NetworkObjectsGroupsComponent
         const data = modal.getData() as NetworkObjectGroup;
 
         if (data !== undefined) {
-          this.saveNetworkObjectGroup(data);
+          throw new Error('Not Implemented');
         }
         this.ngx.resetModalData('networkObjectGroupModal');
       });
-  }
-
-  saveNetworkObject(networkObject: NetworkObject) {
-    if (this.networkObjectModalMode === ModalMode.Create) {
-      this.networkObjects.push(networkObject);
-    } else {
-      this.networkObjects[this.editNetworkObjectIndex] = networkObject;
-    }
   }
 
   deleteNetworkObject(networkObject: NetworkObject) {
     const index = this.networkObjects.indexOf(networkObject);
     if (index > -1) {
       this.networkObjects.splice(index, 1);
-
-      if (!this.deletedNetworkObjects) {
-        this.deletedNetworkObjects = new Array<NetworkObject>();
-      }
-      this.deletedNetworkObjects.push(networkObject);
-
       this.dirty = true;
     }
-  }
-
-  saveNetworkObjectGroup(networkObjectGroup: NetworkObjectGroup) {
-    if (this.networkObjectGroupModalMode === ModalMode.Create) {
-      this.networkObjectGroups.push(networkObjectGroup);
-    } else {
-      this.networkObjectGroups[
-        this.editNetworkObjectGroupIndex
-      ] = networkObjectGroup;
-    }
-    this.dirty = true;
   }
 
   deleteNetworkObjectGroup(networkObjectGroup: NetworkObjectGroup) {
     const index = this.networkObjectGroups.indexOf(networkObjectGroup);
     if (index > -1) {
       this.networkObjectGroups.splice(index, 1);
-
-      if (!this.deletedNetworkObjectGroups) {
-        this.deletedNetworkObjectGroups = new Array<NetworkObjectGroup>();
-      }
-      this.deletedNetworkObjectGroups.push(networkObjectGroup);
-
       this.dirty = true;
     }
-  }
-
-  saveAll() {
-    // TODO: Display warning if objects will be deleted.
-
-    this.dirty = false;
-    const dto = new NetworkObjectDto();
-
-    dto.NetworkObjects = this.networkObjects;
-    dto.NetworkObjectGroups = this.networkObjectGroups;
-    // dto.VrfId = this.currentVrf.id;
-
-    const extra_vars: { [k: string]: any } = {};
-    extra_vars.network_object_dto = dto;
-    extra_vars.vrf_name = this.currentVrf.name.split('-')[1];
-    extra_vars.deleted_network_objects = this.deletedNetworkObjects;
-    extra_vars.deleted_network_object_groups = this.deletedNetworkObjectGroups;
-
-    const body = { extra_vars };
-
-    this.api.launchTemplate('save-network-object-dto', body, true).subscribe(
-      data => {},
-      error => {
-        this.dirty = true;
-      },
-    );
-
-    this.deletedNetworkObjects = new Array<NetworkObject>();
-    this.deletedNetworkObjectGroups = new Array<NetworkObjectGroup>();
   }
 
   private unsubAll() {
