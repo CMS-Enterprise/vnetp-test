dd994a3b1e8bf538e79c010297faee2260e8176d
delps@ineteng.com <delps@ineteng.com>
2019-12-17 22:49:32 -0500

    Initial firewall rule group summary.

--- src/app/components/firewall-rules/firewall-rules.component.ts
+++ src/app/components/firewall-rules/firewall-rules.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { AutomationApiService } from 'src/app/services/automation-api.service';
-import { Vrf } from 'src/app/models/d42/vrf';
-import { HelpersService } from 'src/app/services/helpers.service';
 import { FirewallRulesHelpText } from 'src/app/helptext/help-text-networking';
+import {
+  Tier,
+  V1TiersService,
+  V1DatacentersService,
+  FirewallRuleGroup,
+  FirewallRuleGroupType,
+} from 'api_client';
+import { Subscription } from 'rxjs';
+import { DatacenterContextService } from 'src/app/services/datacenter-context.service';
 
 @Component({
   selector: 'app-firewall-rules',
@@ -11,41 +17,42 @@ import { FirewallRulesHelpText } from 'src/app/helptext/help-text-networking';
 export class FirewallRulesComponent implements OnInit {
   navIndex = 0;
 
-  vrfs: Array<Vrf>;
+  tiers: Array<Tier>;
+  currentDatacenterSubscription: Subscription;
+  firewallRuleGroups: Array<FirewallRuleGroup>;
 
   constructor(
-    private automationApiService: AutomationApiService,
-    private hs: HelpersService,
     public helpText: FirewallRulesHelpText,
+    private datacenterContextService: DatacenterContextService,
+    private tierService: V1TiersService,
   ) {}
 
+  getTiers(dcId: string) {
+    this.tierService
+      .v1DatacentersDatacenterIdTiersGet({
+        datacenterId: dcId,
+        join: 'firewallRuleGroups',
+      })
+      .subscribe(data => {
+        this.tiers = data;
+
+        this.firewallRuleGroups = new Array<FirewallRuleGroup>();
+
+        this.tiers.forEach(tier => {
+          this.firewallRuleGroups = this.firewallRuleGroups.concat(
+            tier.firewallRuleGroups,
+          );
+        });
+      });
+  }
+
   ngOnInit() {
-    this.getVrfs();
-  }
-
-  getVrfs() {
-    this.automationApiService.getVrfs().subscribe(data => (this.vrfs = data));
-  }
-
-  getFirewallRulesCount(object, type) {
-    if (type === 'external') {
-      const firewallRules = this.hs.getJsonCustomField(
-        object,
-        'external_firewall_rules',
-      );
-      return firewallRules ? firewallRules.length : 0;
-    } else if (type === 'intervrf') {
-      const firewallRules = this.hs.getJsonCustomField(
-        object,
-        'firewall_rules',
-      );
-      return firewallRules ? firewallRules.length : 0;
-    } else if (type === 'intravrf') {
-      const contracts = this.hs.getJsonCustomField(
-        object,
-        'intravrf_contracts',
-      );
-      return contracts ? contracts.length : 0;
-    }
+    this.currentDatacenterSubscription = this.datacenterContextService.currentDatacenter.subscribe(
+      cd => {
+        if (cd) {
+          this.getTiers(cd.id);
+        }
+      },
+    );
   }
 }
