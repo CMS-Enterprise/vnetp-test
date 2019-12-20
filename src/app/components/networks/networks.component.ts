f19d929d06be5ea11a3444d296c6f95e829ba4dc
delps@ineteng.com <delps@ineteng.com>
2019-12-20 12:56:50 -0500

    Networks component.

--- src/app/components/networks/networks.component.ts
+++ src/app/components/networks/networks.component.ts
@@ -1,46 +1,76 @@
 import { Component, OnInit } from '@angular/core';
-import { AutomationApiService } from 'src/app/services/automation-api.service';
-import { SubnetResponse, Subnet } from 'src/app/models/d42/subnet';
-import { HelpersService } from 'src/app/services/helpers.service';
+import { Subnet, V1TiersService, Tier, Vlan } from 'api_client';
+import { Subscription } from 'rxjs';
+import { DatacenterContextService } from 'src/app/services/datacenter-context.service';
 
 @Component({
   selector: 'app-networks',
   templateUrl: './networks.component.html',
 })
 export class NetworksComponent implements OnInit {
+  tiers: Array<Tier>;
+  vlans: Array<Vlan>;
+  DatacenterId: string;
+  currentDatacenterSubscription: Subscription;
+
   constructor(
-    private automationApiService: AutomationApiService,
-    private helperService: HelpersService,
-  ) {
-    this.subnets = [];
-  }
+    private tierService: V1TiersService,
+    private datacenterContextService: DatacenterContextService,
+  ) {}
 
   subnets: Array<Subnet>;
 
   ngOnInit() {
-    this.getNetworks();
+    this.currentDatacenterSubscription = this.datacenterContextService.currentDatacenter.subscribe(
+      cd => {
+        if (cd) {
+          this.DatacenterId = cd.id;
+          this.getTiers();
+        }
+      },
+    );
   }
 
-  getNetworks() {
-    this.automationApiService.getSubnets().subscribe(data => {
-      const subnetResponse = data as SubnetResponse;
-      this.subnets = subnetResponse.subnets;
-    });
+  getTiers() {
+    this.tierService
+      .v1DatacentersDatacenterIdTiersGet({
+        datacenterId: this.DatacenterId,
+        join: 'subnets,vlans',
+      })
+      .subscribe(data => {
+        this.tiers = data;
+
+        let subnets = new Array<Subnet>();
+        let vlans = new Array<Vlan>();
+
+        this.tiers.forEach(tier => {
+          subnets = subnets.concat(tier.subnets);
+
+          vlans = vlans.concat(tier.vlans);
+        });
+
+        this.vlans = vlans;
+        this.subnets = subnets;
+      });
   }
 
-  getDeployedState(subnet: Subnet) {
-    return this.helperService.getBooleanCustomField(subnet, 'deployed');
+  getVlanName = (id: string) => {
+    return this.getObjectName(id, this.vlans);
+    // tslint:disable-next-line: semicolon
+  };
+
+  getTierName = (id: string) => {
+    return this.getObjectName(id, this.tiers);
+    // tslint:disable-next-line: semicolon
+  };
+
+  private getObjectName(id: string, objects: { name: string; id?: string }[]) {
+    if (objects && objects.length) {
+      return objects.find(o => o.id === id).name || 'N/A';
+    }
   }
 
-  getTier(subnet: Subnet) {
-    return this.helperService.getStringCustomField(subnet, 'tier');
-  }
+  exportNetworkConfig() {}
 
-  exportNetworkConfig() {
-    return this.subnets;
-  }
-
-  importNetworkConfig(subnets) {
-    throw new Error('Not Implemented');
-  }
+  importNetworkConfig() {}
 }
