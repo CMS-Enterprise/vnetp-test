0febebb2596f87b43e91a98ec56b52b1ac824905
aclarkdev <apclarkdev@gmail.com>
2020-01-31 12:35:18 -0500

    chore: fix specs

--- src/app/modals/virtual-disk-modal/virtual-disk-modal.component.spec.ts
+++ src/app/modals/virtual-disk-modal/virtual-disk-modal.component.spec.ts
@@ -1,14 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VirtualDiskModalComponent } from './virtual-disk-modal.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+import { NgxSmartModalModule, NgxSmartModalService } from 'ngx-smart-modal';
+import { NgxMaskModule } from 'ngx-mask';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgxSmartModalServiceStub } from '../modal-mock';
 
 describe('VirtualDiskModalComponent', () => {
   let component: VirtualDiskModalComponent;
   let fixture: ComponentFixture<VirtualDiskModalComponent>;
 
+  const ngx = new NgxSmartModalServiceStub();
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        AngularFontAwesomeModule,
+        FormsModule,
+        NgxSmartModalModule,
+        ReactiveFormsModule,
+        NgxMaskModule.forRoot(),
+        HttpClientTestingModule,
+      ],
       declarations: [VirtualDiskModalComponent],
+      providers: [
+        { provide: NgxSmartModalService, useValue: ngx },
+        FormBuilder,
+        Validators,
+      ],
     }).compileComponents();
   }));
 
