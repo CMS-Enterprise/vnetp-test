603dcd4986e1077b301b3d4d4b20d81ffebf2069
delps@ineteng.com <delps@ineteng.com>
2020-02-07 20:40:29 -0500

    Initial Deploy Component Refactor

--- src/app/components/deploy/deploy.component.ts
+++ src/app/components/deploy/deploy.component.ts
@@ -1,93 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { Subnet, SubnetResponse } from 'src/app/models/d42/subnet';
-import { HelpersService } from 'src/app/services/helpers.service';
-import { AutomationApiService } from 'src/app/services/automation-api.service';
-import { AuthService } from 'src/app/services/auth.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { V1TiersService, Tier, Datacenter } from 'api_client';
+import { DatacenterContextService } from 'src/app/services/datacenter-context.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-deploy',
   templateUrl: './deploy.component.html',
 })
-export class DeployComponent implements OnInit {
-  tabIndex: number;
-  subnets: Array<Subnet>;
+export class DeployComponent implements OnInit, OnDestroy {
+  currentDatacenterSubscription: Subscription;
+  currentDatacenter: Datacenter;
+
+  navIndex = 0;
+  tiers: Tier[];
 
   constructor(
-    private hs: HelpersService,
-    private automationApiService: AutomationApiService,
-    private auth: AuthService,
-  ) {
-    this.subnets = new Array<Subnet>();
+    private datacenterTiersService: V1TiersService,
+    private datacenterService: DatacenterContextService,
+  ) {}
+
+  getTiers() {
+    this.datacenterTiersService
+      .v1DatacentersDatacenterIdTiersGet({
+        datacenterId: this.currentDatacenter.id,
+      })
+      .subscribe(data => {
+        this.tiers = data;
+      });
   }
 
   ngOnInit() {
-    this.tabIndex = 0;
-    this.getSubnets();
-  }
-
-  getSubnets() {
-    this.automationApiService.getSubnets().subscribe(data => {
-      const subnetResponse = data as SubnetResponse;
-      this.getUndeployedSubnets(subnetResponse.subnets);
-    });
-  }
-
-  getPropertyLength(subnet: Subnet, propertyName: string) {
-    const jsonFirewallRules = subnet.custom_fields.find(
-      c => c.key === propertyName,
+    this.currentDatacenterSubscription = this.datacenterService.currentDatacenter.subscribe(
+      cd => {
+        if (cd) {
+          this.currentDatacenter = cd;
+          this.getTiers();
+        }
+      },
     );
-
-    const firewallRules = JSON.parse(jsonFirewallRules.value);
-
-    let length = 0;
-
-    if (firewallRules != null) {
-      length = firewallRules.length;
-    }
-
-    return length;
   }
 
-  getUndeployedSubnets(subnets: Array<Subnet>) {
-    const undeployedSubnets = [];
-
-    subnets.forEach(subnet => {
-      const deployedState = this.hs.getBooleanCustomField(subnet, 'deployed');
-      if (!deployedState) {
-        undeployedSubnets.push(subnet);
-      }
-    });
-
-    this.subnets = undeployedSubnets;
+  deployTiers() {
+    // TODO: Launch Provisioning Jobs for each Selected Tier.
   }
 
-  deployAll() {
-    this.deployNetwork();
-  }
-
-  deployNetwork() {
-    // Deploy Subnets
-    this.subnets.forEach(s => {
-      this.deploySubnet(s);
-    });
-  }
-  deploySolaris() {
-    const extra_vars: { [k: string]: any } = {};
-    extra_vars.customer_name = this.auth.currentUserValue.CustomerName;
-    const body = { extra_vars };
-    this.automationApiService
-      .launchTemplate('deploy-solaris', body, true)
-      .subscribe();
-  }
-
-  private deploySubnet(subnet: Subnet) {
-    const extra_vars: { [k: string]: any } = {};
-    extra_vars.subnet_id = subnet.subnet_id;
-
-    const body = { extra_vars };
-
-    this.automationApiService
-      .launchTemplate('deploy-network', body, true)
-      .subscribe();
-  }
+  ngOnDestroy() {}
 }
