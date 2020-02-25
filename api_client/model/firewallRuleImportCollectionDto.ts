387bbacd4f62cc7a8f4264580acfdd27d673d5c7
delps@ineteng.com <delps@ineteng.com>
2020-02-25 12:10:17 -0500

    Regenerate API client to remove tierId from firewall rule collection dto.

--- api_client/model/firewallRuleImportCollectionDto.ts
+++ api_client/model/firewallRuleImportCollectionDto.ts
@@ -14,6 +14,7 @@ import { FirewallRuleImport } from './firewallRuleImport';
 
 export interface FirewallRuleImportCollectionDto { 
     datacenterId: string;
+    tierId: string;
     firewallRules: Array<FirewallRuleImport>;
 }
 
