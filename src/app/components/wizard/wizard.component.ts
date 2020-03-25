1efb5ec2ba285520e724831518fcda661bf8ef62
aclarkdev <apclarkdev@gmail.com>
2020-03-25 18:07:26 -0400

    chore: generate client and setup calls to vts

--- src/app/components/wizard/wizard.component.ts
+++ src/app/components/wizard/wizard.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { WizardSection } from 'src/app/models/wizard/wizard-data';
-import { PieChartData } from '../d3-pie-chart/d3-pie-chart.component';
 import { pieChartData, lineChartData, pieData } from './mockChartData';
+import { V1VtsService } from 'api_client';
+import { Papa } from 'ngx-papaparse';
 @Component({
   selector: 'app-wizard',
   templateUrl: './wizard.component.html',
@@ -13,11 +14,17 @@ export class WizardComponent implements OnInit {
   pieChartData: any;
   lineChartData: any;
 
+  replicationQueueDepth: any;
+  replicationsNotCompleted: any;
+
+  formattedQueueDepth: [];
+  formattedReplicationData: any;
+
   // options
   pieView: any[] = [700, 300];
-  pieShowLegend: boolean = true;
-  pieShowLabels: boolean = true;
-  legendPosition: string = 'below';
+  pieShowLegend = true;
+  pieShowLabels = true;
+  legendPosition = 'below';
 
   pieColorScheme = {
     domain: ['#e84d4d', '#5ac4f9', '#ffdf5a'],
@@ -25,36 +32,110 @@ export class WizardComponent implements OnInit {
 
   // line chart options
   view: any[] = [700, 275];
-  legend: boolean = true;
-  showLabels: boolean = true;
-  animations: boolean = true;
-  xAxis: boolean = true;
-  yAxis: boolean = true;
-  showYAxisLabel: boolean = true;
-  showXAxisLabel: boolean = true;
-  xAxisLabel: string = 'Time';
-  yAxisLabel: string = 'Number Queued';
-  timeline: boolean = false;
-  yScaleMax = 5;
+  legend = true;
+  showLabels = true;
+  animations = true;
+  xAxis = true;
+  yAxis = true;
+  showYAxisLabel = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Time';
+  yAxisLabel = 'Number Queued';
+  timeline = false;
+  yScaleMax = 150;
   colorScheme = {
     domain: ['#ffad00'],
   };
 
-  constructor() {}
+  constructor(private vtsService: V1VtsService, private papa: Papa) {}
 
   getProgressBarPercentage(statusProgress: number) {
     return `${statusProgress}%`;
   }
 
+  countStatuses(status: string) {
+    switch (status) {
+      case 'error':
+        this.pieChartData[0].value = this.pieChartData[0].value + 1;
+        break;
+      case 'write':
+        this.pieChartData[1].value = this.pieChartData[1].value + 1;
+        break;
+      case 'queued':
+        this.pieChartData[2].value = this.pieChartData[2].value + 1;
+        break;
+      default:
+        throw new Error('Invalid Status Type');
+    }
+  }
+
   ngOnInit() {
+    this.vtsService.v1VtsReplicationQueueDepthPost().subscribe(data => {
+      this.replicationQueueDepth = data;
+      const options = {
+        skipEmptyLines: true,
+        header: true,
+      };
+      let parsedData = null;
+      let papaParsedData = null;
+      if (this.replicationQueueDepth) {
+        parsedData = JSON.parse(JSON.stringify(this.replicationQueueDepth));
+        papaParsedData = this.papa.parse(parsedData, options);
+        console.log(papaParsedData);
+        this.replicationQueueDepth = [{ name: 'Replication Queue Depth', series: [] }];
+        papaParsedData.data.map(item => {
+          this.replicationQueueDepth[0].series.push({
+            name: new Date(Number(item.Timestamp)),
+            value: item['Queue Depth'],
+          });
+        });
+        this.lineChartData = this.replicationQueueDepth;
+      }
+    });
+
+    this.vtsService.v1VtsReplicationNotCompletedPost().subscribe(data => {
+      this.replicationsNotCompleted = data;
+      const options = {
+        skipEmptyLines: true,
+        header: true,
+      };
+      let parsedData = null;
+      let papaParsedData = null;
+      if (this.replicationsNotCompleted) {
+        parsedData = JSON.parse(JSON.stringify(this.replicationsNotCompleted));
+        papaParsedData = this.papa.parse(parsedData, options);
+        console.log(papaParsedData);
+        this.pieChartData = [
+          {
+            name: 'error',
+            value: 0,
+          },
+          {
+            name: 'write',
+            value: 0,
+          },
+          {
+            name: 'queued',
+            value: 0,
+          },
+        ];
+        this.formattedReplicationData = [];
+        papaParsedData.data.map(item => {
+          const replicationData = item.Date.split(' ');
+          this.formattedReplicationData.push({
+            date: `${replicationData[0]} ${replicationData[1]}`,
+            status: `${replicationData[2]}`,
+            volser: `${replicationData[3]}`,
+            storagePool: `${replicationData[4]}`,
+            cgx: `${replicationData[5]}`,
+          });
+          this.countStatuses(replicationData[2]);
+        });
+        console.log(this.pieChartData);
+      }
+    });
     this.pieChartData = pieChartData;
     this.lineChartData = lineChartData;
-
-    this.lineChartData.map(item => {
-      item.series.map(d => {
-        d.name = new Date(d.name * 1000);
-      });
-    });
 
     this.WizardSections = [
       {
