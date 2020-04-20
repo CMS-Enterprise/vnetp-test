3cafdfd0ec48248efc87e470964fb30f2f633181
delps@ineteng.com <delps@ineteng.com>
2020-04-20 13:34:20 -0400

    Fix appliance form validator.

--- src/app/modals/appliance-modal/appliance-modal.component.ts
+++ src/app/modals/appliance-modal/appliance-modal.component.ts
@@ -168,7 +168,7 @@ export class ApplianceModalComponent implements OnInit, OnDestroy {
     this.form = this.formBuilder.group({
       name: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(100), NameValidator])],
       description: ['', Validators.compose([Validators.minLength(3), Validators.maxLength(500)])],
-      rackUnits: ['', Validators.required],
+      rackUnits: [0, Validators.compose([Validators.required, Validators.min(1)])],
       serialNumber: ['', Validators.required],
       deliveryDate: ['', Validators.required],
       localStorageType: ['', Validators.required],
