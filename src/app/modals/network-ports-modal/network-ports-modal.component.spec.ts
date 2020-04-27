20fef570a9ed381d6b974d6118c599f440232610
delps@ineteng.com <delps@ineteng.com>
2020-04-27 17:54:46 -0400

    Fix network ports modal spec.

--- src/app/modals/network-ports-modal/network-ports-modal.component.spec.ts
+++ src/app/modals/network-ports-modal/network-ports-modal.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NetworkPortsModalComponent } from './network-ports-modal.component';
+import { NgxSmartModalModule, NgxSmartModalService } from 'ngx-smart-modal';
+import { NgxSmartModalServiceStub } from '../modal-mock';
 
 describe('NetworkPortsModalComponent', () => {
   let component: NetworkPortsModalComponent;
   let fixture: ComponentFixture<NetworkPortsModalComponent>;
 
+  const ngx = new NgxSmartModalServiceStub();
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [NgxSmartModalModule],
       declarations: [NetworkPortsModalComponent],
+      providers: [{ provide: NgxSmartModalService, useValue: ngx }],
     }).compileComponents();
   }));
 
