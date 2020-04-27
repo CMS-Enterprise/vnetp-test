f08fa5c064edc785fbb139baee212783e72ae27b
delps@ineteng.com <delps@ineteng.com>
2020-04-27 17:47:57 -0400

    Fix zLinux related component tests.

--- src/app/components/zvm/zvm.component.spec.ts
+++ src/app/components/zvm/zvm.component.spec.ts
@@ -1,14 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ZvmComponent } from './zvm.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { NgxSmartModalServiceStub } from 'src/app/modals/modal-mock';
+import { NgxSmartModalService, NgxSmartModalModule } from 'ngx-smart-modal';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { ZosZvmRequestModalComponent } from 'src/app/modals/zos-zvm-request-modal/zos-zvm-request-modal.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ZvmComponent', () => {
   let component: ZvmComponent;
   let fixture: ComponentFixture<ZvmComponent>;
 
+  const ngx = new NgxSmartModalServiceStub();
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ZvmComponent],
+      imports: [
+        AngularFontAwesomeModule,
+        NgxPaginationModule,
+        NgxSmartModalModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+      ],
+      declarations: [ZvmComponent, ZosZvmRequestModalComponent],
+      providers: [{ provide: NgxSmartModalService, useValue: ngx }],
     }).compileComponents();
   }));
 
