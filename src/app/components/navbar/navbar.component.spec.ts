8febe647deaadcbee351401cb54df89a86afd9b2
Cody Cahoon <codyjcahoon@gmail.com>
2020-07-01 09:57:23 -0400

    test: Adds tests for navbar component

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,15 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavbarComponent } from './navbar.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { MockFontAwesomeComponent } from 'src/test/mock-components';
-import { NgxSmartModalModule, NgxSmartModalService } from 'ngx-smart-modal';
+import { MockFontAwesomeComponent, MockNgxSmartModalComponent } from 'src/test/mock-components';
+import { NgxSmartModalService, NgxSmartModalComponent } from 'ngx-smart-modal';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NgxSmartModalServiceStub } from 'src/app/modals/modal-mock';
 import { FormsModule } from '@angular/forms';
 import { FilterPipe } from 'src/app/pipes/filter.pipe';
-import { ToastrModule } from 'ngx-toastr';
+import { User } from 'src/app/models/user/user';
+import { AutomationApiService } from 'src/app/services/automation-api.service';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
@@ -20,8 +23,8 @@ describe('NavbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, NgxSmartModalModule, ToastrModule.forRoot({}), FormsModule, HttpClientTestingModule],
-      declarations: [NavbarComponent, FilterPipe, MockFontAwesomeComponent],
+      imports: [RouterTestingModule, FormsModule, HttpClientTestingModule],
+      declarations: [NavbarComponent, FilterPipe, MockFontAwesomeComponent, MockNgxSmartModalComponent],
       providers: [CookieService, { provide: NgxSmartModalService, useValue: ngx }],
     })
       .compileComponents()
@@ -39,5 +42,27 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('getJobs', () => {
+    it('should get jobs for the current user', () => {
+      component.currentUser = {} as User;
+
+      const automationApiService = TestBed.get(AutomationApiService);
+      const getJobsSpy = spyOn(automationApiService, 'getJobs').and.returnValue(of({}));
+
+      component.getJobs();
+      expect(getJobsSpy).toHaveBeenCalled();
+    });
+
+    it('should not get jobs in there is not a current user', () => {
+      component.currentUser = null;
+
+      const automationApiService = TestBed.get(AutomationApiService);
+      const getJobsSpy = spyOn(automationApiService, 'getJobs').and.returnValue(of({}));
+
+      component.getJobs();
+      expect(getJobsSpy).not.toHaveBeenCalled();
+    });
   });
 });
