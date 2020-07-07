f4563c08818c67d76dbee6a93fb96984350bfc5c
Cody Cahoon <codyjcahoon@gmail.com>
2020-07-07 11:11:59 -0400

    test: Adds tests for virtual-disk-modal component

--- src/app/modals/virtual-disk-modal/virtual-disk-modal.component.spec.ts
+++ src/app/modals/virtual-disk-modal/virtual-disk-modal.component.spec.ts
@@ -1,83 +1,203 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { VirtualDiskModalComponent } from './virtual-disk-modal.component';
-import { MockFontAwesomeComponent } from 'src/test/mock-components';
+import { MockFontAwesomeComponent, MockNgxSmartModalComponent } from 'src/test/mock-components';
 import { FormsModule, ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
-import { NgxSmartModalModule, NgxSmartModalService } from 'ngx-smart-modal';
-import { NgxMaskModule } from 'ngx-mask';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgxSmartModalService } from 'ngx-smart-modal';
 import { NgxSmartModalServiceStub } from '../modal-mock';
+import { V1VmwareVirtualDisksService } from 'api_client';
+import TestUtil from 'src/test/test.util';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
+import { VirtualMachineModalDto } from 'src/app/models/vmware/virtual-machine-modal-dto';
+import { ModalMode } from 'src/app/models/other/modal-mode';
 
 describe('VirtualDiskModalComponent', () => {
   let component: VirtualDiskModalComponent;
   let fixture: ComponentFixture<VirtualDiskModalComponent>;
 
-  const ngx = new NgxSmartModalServiceStub();
-
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [FormsModule, NgxSmartModalModule, ReactiveFormsModule, NgxMaskModule.forRoot(), HttpClientTestingModule],
-      declarations: [VirtualDiskModalComponent, MockFontAwesomeComponent],
-      providers: [{ provide: NgxSmartModalService, useValue: ngx }, FormBuilder, Validators],
-    }).compileComponents();
-  }));
+    const ngx = new NgxSmartModalServiceStub();
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(VirtualDiskModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    const virtualDiskService = {
+      v1VmwareVirtualDisksPost: jest.fn(() => of({})),
+    };
+
+    TestBed.configureTestingModule({
+      imports: [FormsModule, ReactiveFormsModule],
+      declarations: [VirtualDiskModalComponent, MockFontAwesomeComponent, MockNgxSmartModalComponent],
+      providers: [
+        { provide: NgxSmartModalService, useValue: ngx },
+        { provide: V1VmwareVirtualDisksService, useValue: virtualDiskService },
+        FormBuilder,
+        Validators,
+      ],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(VirtualDiskModalComponent);
+        component = fixture.componentInstance;
+        component.VirtualMachineId = '1';
+        fixture.detectChanges();
+      });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  // Name validity
-  it('name should be valid', () => {
-    const name = component.form.controls.name;
-    name.setValue('a'.repeat(3));
-    expect(name.valid).toBeTruthy();
+  describe('Name', () => {
+    it('should be valid', () => {
+      const name = component.form.controls.name;
+      name.setValue('a'.repeat(3));
+      expect(name.valid).toBeTruthy();
+    });
+
+    it('should be invalid, min length', () => {
+      const name = component.form.controls.name;
+      name.setValue('a'.repeat(2));
+      expect(name.valid).toBeFalsy();
+    });
+
+    it('should be invalid, max length', () => {
+      const name = component.form.controls.name;
+      name.setValue('a'.repeat(101));
+      expect(name.valid).toBeFalsy();
+    });
+
+    it('should be invalid, invalid characters', () => {
+      const name = component.form.controls.name;
+      name.setValue('invalid/name!');
+      expect(name.valid).toBeFalsy();
+    });
   });
 
-  it('name should be invalid, min length', () => {
-    const name = component.form.controls.name;
-    name.setValue('a'.repeat(2));
-    expect(name.valid).toBeFalsy();
+  describe('Description', () => {
+    it('should be valid (null)', () => {
+      const description = component.form.controls.description;
+      description.setValue(null);
+      expect(description.valid).toBeTruthy();
+    });
+
+    it('should be valid (minlen)', () => {
+      const description = component.form.controls.description;
+      description.setValue('a'.repeat(3));
+      expect(description.valid).toBeTruthy();
+    });
+
+    it('should be invalid, min length', () => {
+      const description = component.form.controls.description;
+      description.setValue('a'.repeat(2));
+      expect(description.valid).toBeFalsy();
+    });
+
+    it('should be invalid, max length', () => {
+      const description = component.form.controls.description;
+      description.setValue('a'.repeat(501));
+      expect(description.valid).toBeFalsy();
+    });
   });
 
-  it('name should be invalid, max length', () => {
-    const name = component.form.controls.name;
-    name.setValue('a'.repeat(101));
-    expect(name.valid).toBeFalsy();
+  it('should require name', () => {
+    const requiredFields = ['name'];
+
+    requiredFields.forEach(f => {
+      expect(TestUtil.isFormControlRequired(component.form.controls[f])).toBe(true);
+    });
   });
 
-  it('name should be invalid, invalid characters', () => {
-    const name = component.form.controls.name;
-    name.setValue('invalid/name!');
-    expect(name.valid).toBeFalsy();
+  it('should not require description', () => {
+    const optionalFields = ['description'];
+
+    optionalFields.forEach(f => {
+      expect(TestUtil.isFormControlRequired(component.form.controls[f])).toBe(false);
+    });
   });
 
-  // Description Validity
-  it('description should be valid (null)', () => {
-    const description = component.form.controls.description;
-    description.setValue(null);
-    expect(description.valid).toBeTruthy();
+  it('should return form controls', () => {
+    expect(component.f.name).toBeTruthy();
   });
 
-  it('description should be valid (minlen)', () => {
-    const description = component.form.controls.description;
-    description.setValue('a'.repeat(3));
-    expect(description.valid).toBeTruthy();
+  it('should reset the form when closing the modal', () => {
+    component.form.controls.name.setValue('Test');
+
+    const cancelButton = fixture.debugElement.query(By.css('.btn.btn-link'));
+    cancelButton.nativeElement.click();
+
+    expect(component.form.controls.name.value).toBe('');
   });
 
-  it('description should be invalid, min length', () => {
-    const description = component.form.controls.description;
-    description.setValue('a'.repeat(2));
-    expect(description.valid).toBeFalsy();
+  describe('save', () => {
+    const clickSaveButton = () => fixture.debugElement.query(By.css('.btn.btn-success')).nativeElement.click();
+
+    it('should not create a vmware virtual disk when the form is invalid', () => {
+      const service = TestBed.get(V1VmwareVirtualDisksService);
+      const createVirtualDiskSpy = jest.spyOn(service, 'v1VmwareVirtualDisksPost');
+
+      component.form.setValue({
+        name: null,
+        description: 'Description',
+        diskSize: null,
+        rawLun: null,
+      });
+
+      clickSaveButton();
+
+      expect(createVirtualDiskSpy).not.toHaveBeenCalled();
+    });
+
+    it('should create a vmware virtual disk', () => {
+      const service = TestBed.get(V1VmwareVirtualDisksService);
+      const createVirtualDiskSpy = jest.spyOn(service, 'v1VmwareVirtualDisksPost');
+
+      component.form.setValue({
+        name: 'Test',
+        description: 'Description',
+        diskSize: 1,
+        rawLun: true,
+      });
+
+      clickSaveButton();
+
+      expect(createVirtualDiskSpy).toHaveBeenCalledWith({
+        vmwareVirtualDisk: {
+          virtualMachineId: '1',
+          name: 'Test',
+          description: 'Description',
+          diskSize: 1000000000,
+          rawLun: true,
+        },
+      });
+    });
   });
 
-  it('description should be invalid, max length', () => {
-    const description = component.form.controls.description;
-    description.setValue('a'.repeat(501));
-    expect(description.valid).toBeFalsy();
+  describe('getData', () => {
+    const createDto = (): VirtualMachineModalDto => {
+      return {
+        DatacenterId: '1',
+        VirtualMachineId: '2',
+        ModalMode: ModalMode.Create,
+        VmwareVirtualMachine: {
+          id: '24',
+          datacenterId: '1',
+          name: 'Virtual Machine',
+          cpuCores: 2,
+          cpuCoresPerSocket: 2,
+          cpuReserved: true,
+          memoryReserved: true,
+          memorySize: 2,
+        },
+      };
+    };
+
+    it('should initialize the virtual machine', () => {
+      const service = TestBed.get(NgxSmartModalService);
+      const dto = createDto();
+      dto.VirtualMachineId = '123';
+      jest.spyOn(service, 'getModalData').mockImplementation(() => dto);
+
+      component.getData();
+
+      expect(component.VirtualMachineId).toBe('123');
+    });
   });
 });
--- src/app/modals/virtual-disk-modal/virtual-disk-modal.component.ts
+++ src/app/modals/virtual-disk-modal/virtual-disk-modal.component.ts
@@ -21,62 +21,56 @@ export class VirtualDiskModalComponent implements OnInit {
     private virtualDiskService: V1VmwareVirtualDisksService,
   ) {}
 
-  save() {
-    if (this.form.invalid) {
-      return;
-    }
-
-    const virtualDisk = {} as VmwareVirtualDisk;
-    virtualDisk.name = this.form.value.name;
-    virtualDisk.description = this.form.value.description;
-    virtualDisk.diskSize = ConversionUtil.convertGbToBytes(this.form.value.diskSize);
-    virtualDisk.rawLun = this.form.value.rawLun;
-    virtualDisk.virtualMachineId = this.VirtualMachineId;
-
-    this.ngx.resetModalData('virtualDiskModal');
-    this.ngx.setModalData(Object.assign({}, virtualDisk), 'virtualDiskModal');
-
-    this.virtualDiskService
-      .v1VmwareVirtualDisksPost({
-        vmwareVirtualDisk: virtualDisk,
-      })
-      .subscribe(
-        data => {
-          this.closeModal();
-        },
-        error => {},
-      );
-  }
-
-  getData() {
-    const dto = Object.assign({}, this.ngx.getModalData('virtualDiskModal') as VirtualMachineModalDto);
-    this.VirtualMachineId = dto.VirtualMachineId;
-  }
-
-  cancel() {
-    this.closeModal();
-  }
-
   get f() {
     return this.form.controls;
   }
 
-  private buildForm() {
+  public closeModal(): void {
+    this.ngx.close('virtualDiskModal');
+    this.reset();
+  }
+
+  public save(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const { name, description, diskSize, rawLun } = this.form.value;
+    const vmwareVirtualDisk = {
+      name,
+      description,
+      rawLun,
+      diskSize: ConversionUtil.convertGbToBytes(diskSize),
+      virtualMachineId: this.VirtualMachineId,
+    } as VmwareVirtualDisk;
+
+    this.ngx.resetModalData('virtualDiskModal');
+    this.ngx.setModalData(Object.assign({}, vmwareVirtualDisk), 'virtualDiskModal');
+
+    this.virtualDiskService.v1VmwareVirtualDisksPost({ vmwareVirtualDisk }).subscribe(
+      data => {
+        this.closeModal();
+      },
+      error => {},
+    );
+  }
+
+  public getData(): void {
+    const dto = Object.assign({}, this.ngx.getModalData('virtualDiskModal') as VirtualMachineModalDto);
+    this.VirtualMachineId = dto.VirtualMachineId;
+  }
+
+  public reset(): void {
+    this.buildForm();
+  }
+
+  private buildForm(): void {
     this.form = this.formBuilder.group({
       name: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(100), NameValidator])],
       description: ['', Validators.compose([Validators.minLength(3), Validators.maxLength(500)])],
       diskSize: [''],
       rawLun: [''],
     });
-  }
-
-  private closeModal() {
-    this.ngx.close('virtualDiskModal');
-    this.reset();
-  }
-
-  public reset() {
-    this.buildForm();
   }
 
   ngOnInit() {
