05ffb4a1baf5fb30dfd707d779ccd9401185167a
Cody Cahoon <codyjcahoon@gmail.com>
2020-07-24 14:02:03 -0400

    chore: Removes unused d3-graph component

--- src/app/common/shared.module.ts
+++ src/app/common/shared.module.ts
@@ -4,7 +4,6 @@ import { YesNoModalComponent } from 'src/app/common/yes-no-modal/yes-no-modal.co
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgxSmartModalModule } from 'ngx-smart-modal';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { D3GraphComponent } from './d3-graph/d3-graph.component';
 import { D3PieChartComponent } from './d3-pie-chart/d3-pie-chart.component';
 import { ImportExportComponent } from './import-export/import-export.component';
 import { TierSelectComponent } from './tier-select/tier-select.component';
@@ -27,7 +26,6 @@ import { ResolvePipe } from '../pipes/resolve.pipe';
     NgxPaginationModule,
   ],
   declarations: [
-    D3GraphComponent,
     D3PieChartComponent,
     ImportExportComponent,
     ResolvePipe,
@@ -43,7 +41,6 @@ import { ResolvePipe } from '../pipes/resolve.pipe';
     ReactiveFormsModule,
     NgxSmartModalModule,
     NgxPaginationModule,
-    D3GraphComponent,
     D3PieChartComponent,
     ImportExportComponent,
     ResolvePipe,
