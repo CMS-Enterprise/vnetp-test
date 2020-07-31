2c430c2c3706f56b2e0ef32a0a8426cc1afd1ee7
aclarkdev <apclarkdev@gmail.com>
2020-07-31 15:19:01 -0400

    chore: consume table as module

--- src/app/common/preview-modal/preview-modal.module.ts
+++ src/app/common/preview-modal/preview-modal.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PreviewModalComponent } from './preview-modal.component';
-import { TableComponent } from '../table/table.component';
 import { NgxSmartModalModule } from 'ngx-smart-modal';
+import { TableModule } from '../table/table.module';
 
 @NgModule({
-  imports: [CommonModule, NgxSmartModalModule],
-  declarations: [PreviewModalComponent, TableComponent],
+  imports: [CommonModule, NgxSmartModalModule, TableModule],
+  declarations: [PreviewModalComponent],
   exports: [PreviewModalComponent],
 })
 export class PreviewModalModule {}
