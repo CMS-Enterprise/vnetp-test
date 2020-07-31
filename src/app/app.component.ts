d4e4862fb2802e64b7af4f653a1d6a634c339bc0
Cody Cahoon <codyjcahoon@gmail.com>
2020-07-31 09:31:35 -0400

    feature: Changes title of page when route changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter, map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private title: Title) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map(route => {
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+          return route;
+        }),
+        filter(route => route.outlet === 'primary'),
+        mergeMap(route => route.data),
+      )
+      .subscribe(event => {
+        this.title.setTitle(event.title || 'Automation');
+      });
+  }
+}
