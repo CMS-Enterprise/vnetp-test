260ed066504b8c2e91716a0d45ef2acc67232a4a
Cody Cahoon <codyjcahoon@gmail.com>
2020-08-07 11:20:35 -0400

    feature: Adds initial components for priority group

---
+++ src/app/components/vmware/priority-group/priority-group-list/priority-group-list.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-priority-group-list',
+  templateUrl: './priority-group-list.component.html',
+})
+export class PriorityGroupListComponent {}
---
+++ src/app/components/vmware/priority-group/priority-group-modal/priority-group-modal.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-priority-group-modal',
+  templateUrl: './priority-group-modal.component.html',
+})
+export class PriorityGroupModalComponent {}
