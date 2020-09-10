bc1989c935e221c66c6aef05635729d4adcf5a96
Cody Cahoon <codyjcahoon@gmail.com>
2020-09-10 16:06:54 -0400

    refactor: Removes unused Solaris code

--- src/app/models/interfaces/solaris-load-balancer.interface.ts
+++
@@ -1,8 +0,0 @@
-import { SolarisLdom } from '../solaris/solaris-ldom';
-
-export interface SolarisLdomResponse {
-  total_count: number;
-  offset: number;
-  limit: number;
-  Devices: SolarisLdom[];
-}
--- src/app/services/automation-api.service.ts
+++ src/app/services/automation-api.service.ts
@@ -7,25 +7,13 @@ import { AppMessage } from '../models/app-message';
 import { AppMessageType } from '../models/app-message-type';
 import { MessageService } from './message.service';
 import { map, catchError } from 'rxjs/operators';
-import { SolarisImage } from 'src/app/models/solaris/solaris-image';
 import { throwError } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
 export class AutomationApiService {
   constructor(private http: HttpClient, private auth: AuthService, private ms: MessageService) {}
-
-  getJobs(query?: string) {
-    if (query == null) {
-      query = '';
-    }
-
-    return this.http.get(environment.apiBase + '/api/v2/jobs/' + query);
-  }
-
-  getJob(id: number) {
-    return this.http.get(environment.apiBase + '/api/v2/jobs/' + `${id}/`);
-  }
 
   launchTemplate(jobName: string, ansibleBody, sendJobLaunchMessage = false) {
     const fullJobName = `${this.auth.currentUserValue.CustomerIdentifier}-${jobName}`;
@@ -44,62 +32,6 @@ export class AutomationApiService {
     );
   }
 
-  getAdminGroups() {
-    return this.http.get(environment.apiBase + '/api/1.0/admingroups/');
-  }
-
-  doqlQuery(query: string) {
-    return this.http.get(environment.apiBase + '/services/data/v1.0/query/?query=' + query + '&output_type=json');
-  }
-
-  getIps() {
-    return this.http.get(environment.apiBase + '/api/1.0/ips/');
-  }
-
-  getIpNats() {
-    return this.http.get(environment.apiBase + '/api/1.0/ipnat/');
-  }
-
-  getDevicesbyID(id: string) {
-    return this.http.get(environment.apiBase + `/api/1.0/devices/${id}/`);
-  }
-  getDevices() {
-    return this.http.get(environment.apiBase + `/api/1.0/devices/`);
-  }
-  getDevicesbyName(name: string) {
-    return this.http.get(environment.apiBase + `/api/1.0/devices/?name=${name}`);
-  }
-  getCDoms() {
-    return this.http.get(environment.apiBase + `/api/1.0/devices/?custom_fields_and=DeviceType:solaris_cdom`);
-  }
-
-  getLDoms() {
-    return this.http.get(environment.apiBase + `/api/1.0/devices/?custom_fields_and=DeviceType:solaris_ldom`);
-  }
-  getLDomByName(name: string) {
-    return this.http.get(environment.apiBase + `/api/1.0/devices/?custom_fields_and=DeviceType:solaris_ldom&virtual_host_name=${name}`);
-  }
-  getCDomByName(name: string) {
-    return this.http.get(environment.apiBase + `/api/1.0/devices/?custom_fields_and=DeviceType:solaris_cdom&virtual_host_name=${name}`);
-  }
-
-  getLDomsForCDom(name: string) {
-    return this.http.get(environment.apiBase + `/api/1.0/devices/?custom_fields_and=DeviceType:solaris_ldom&virtual_host_name=${name}`);
-  }
-  getCDomByID(id: any) {
-    return this.http.get(environment.apiBase + `/api/1.0/devices/?custom_fields_and=DeviceType:solaris_cdom&device_id=${id}`);
-  }
-  getSolarisImages(name: string) {
-    return this.http.get(environment.apiBase + `/api/1.0/parts/?device=${name}`);
-  }
-  getSolarisImageDetail(id: any) {
-    return this.http.get<SolarisImage[]>(environment.apiBase + `/api/1.0/parts/?device_id=${id}`);
-  }
-
-  getVrfs() {
-    return this.http.get<Vrf[]>(environment.apiBase + '/api/1.0/vrf_group/');
-  }
-
   getVrf(id: any) {
     return (
       this.http
@@ -112,27 +44,5 @@ export class AutomationApiService {
           }),
         )
     );
-  }
-
-  getSubnets(vrfId?: number) {
-    let uri = '/api/1.0/subnets';
-
-    if (vrfId) {
-      uri += `?vrf_group_id=${vrfId}`;
-    }
-
-    return this.http.get(environment.apiBase + uri);
-  }
-
-  getSubnet(id: string) {
-    return this.http.get(environment.apiBase + `/api/1.0/subnets/${id}`);
-  }
-
-  getSubnetIps(id: number) {
-    return this.http.get(environment.apiBase + `/api/1.0/ips/subnet_id/${id}`);
-  }
-
-  getSystemStatus() {
-    return this.http.get(environment.apiBase + '/api/status');
   }
 }
