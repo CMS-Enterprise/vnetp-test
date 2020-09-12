b5d4767bc75fb6f9a3371a642df6d30d1144fb1e
aclarkdev <apclarkdev@gmail.com>
2020-09-12 15:47:36 -0400

    chore: fix spec

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,23 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service';
-import { CookieService } from 'ngx-cookie-service';
-import { of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeAll(() => {
-    const cookieService = {
-      deleteAll: jest.fn(),
-    };
-
-    const http = {
-      post: jest.fn(() => of({ token: '' })),
-    };
-
     TestBed.configureTestingModule({
-      providers: [AuthService, { provide: CookieService, useValue: cookieService }, { provide: HttpClient, useValue: http }],
+      providers: [AuthService],
     });
     service = TestBed.get(AuthService);
   });
