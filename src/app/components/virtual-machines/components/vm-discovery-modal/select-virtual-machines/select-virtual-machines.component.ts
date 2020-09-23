f31476122415cfd30ac2d97b721f752ccbb74347
Cody Cahoon <cody.cahoon@levvel.io>
2020-09-23 10:22:33 -0400

    refactor: Creates components for different steps of discovery process

---
+++ src/app/components/virtual-machines/components/vm-discovery-modal/select-virtual-machines/select-virtual-machines.component.ts
@@ -0,0 +1,71 @@
+import { Component, Input, OnInit, Output, TemplateRef, ViewChild, EventEmitter } from '@angular/core';
+import { ActifioApplicationDto, V1AgmHostsService } from 'api_client';
+import { NgxSmartModalService } from 'ngx-smart-modal';
+import { TableConfig } from 'src/app/common/table/table.component';
+
+interface SelectableVirtualMachine extends ActifioApplicationDto {
+  isSelected: boolean;
+}
+
+@Component({
+  selector: 'app-select-virtual-machines',
+  templateUrl: './select-virtual-machines.component.html',
+  styles: ['.loading { display: flex; flex-direction: column; align-items: center'],
+})
+export class SelectVirtualMachinesComponent implements OnInit {
+  @ViewChild('selectVirtualMachineToggleTemplate', { static: false }) selectVirtualMachineToggleTemplate: TemplateRef<any>;
+
+  @Input() vcenterId: number;
+  @Output() virtualMachinesSelected = new EventEmitter<Set<string>>();
+
+  public config: TableConfig = {
+    description: 'List of Virtual Machines on vCenter',
+    columns: [
+      { name: '', template: () => this.selectVirtualMachineToggleTemplate },
+      { name: 'Name', property: 'name' },
+      { name: 'Folder Path', property: 'folderPath' },
+      { name: 'Managed?', property: 'isManaged' },
+    ],
+  };
+  public isLoading = false;
+  public selectedVirtualMachineIds = new Set<string>();
+  public selectableVirtualMachines: SelectableVirtualMachine[] = [];
+
+  constructor(private ngx: NgxSmartModalService, private agmHostService: V1AgmHostsService) {}
+
+  ngOnInit(): void {
+    this.loadVirtualMachinesOnHost(this.vcenterId);
+  }
+
+  public onCancel(): void {
+    this.ngx.close('vmDiscoveryModal');
+  }
+
+  public selectVirtualMachines(): void {
+    if (this.selectedVirtualMachineIds.size === 0) {
+      return;
+    }
+    this.virtualMachinesSelected.emit(this.selectedVirtualMachineIds);
+  }
+
+  public selectVirtualMachine(id: string): void {
+    if (this.selectedVirtualMachineIds.has(id)) {
+      this.selectedVirtualMachineIds.delete(id);
+    } else {
+      this.selectedVirtualMachineIds.add(id);
+    }
+  }
+
+  public loadVirtualMachinesOnHost(hostId: number): void {
+    this.isLoading = true;
+    this.agmHostService.v1AgmHostsHostIdDiscoveredApplicationsGet({ hostId }).subscribe((data: ActifioApplicationDto[]) => {
+      this.selectableVirtualMachines = data.map(application => {
+        return {
+          ...application,
+          isSelected: false,
+        };
+      });
+      this.isLoading = false;
+    });
+  }
+}
