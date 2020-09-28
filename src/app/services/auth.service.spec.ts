33dc326b964e791a247f2007c004fded82982cb9
aclarkdev <apclarkdev@gmail.com>
2020-09-28 12:00:58 -0400

    chore: fix spec imports

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
@@ -6,6 +7,7 @@ describe('AuthService', () => {
 
   beforeAll(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       providers: [AuthService],
     });
     service = TestBed.get(AuthService);
