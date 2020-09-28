98753bd6305a048319dc80a8cc5ca02a69ffc56c
Jim Van Fleet <jim.van.fleet@levvel.io>
2020-09-28 14:28:51 -0400

    Blank API base

--- src/environments/environment.prod-ci.ts
+++ src/environments/environment.prod-ci.ts
@@ -19,7 +19,7 @@ const openId: OidcClientSettings = {
 
 export const environment = {
   production: true,
-  apiBase: 'http://localhost:3000',
+  apiBase: '',
   wikiBase: 'http://wiki.draas.cdsvdc.lcl/index.php/UI',
   openId,
 };
