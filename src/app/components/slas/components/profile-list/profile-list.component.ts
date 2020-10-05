b7c3739ed344129327a2eeba086e2807f88053c4
Cody Cahoon <cody.cahoon@levvel.io>
2020-10-05 14:11:33 -0400

    feature: Adds more fields to SLA Profile list page

--- src/app/components/slas/components/profile-list/profile-list.component.ts
+++ src/app/components/slas/components/profile-list/profile-list.component.ts
@@ -1,5 +1,10 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActifioProfileDto, V1AgmProfilesService } from 'api_client';
+
+interface ProfileView extends ActifioProfileDto {
+  mostRecentChangeDate: string;
+}
 
 @Component({
   selector: 'app-profile-list',
@@ -13,12 +18,28 @@ export class ProfileListComponent implements OnInit {
         name: 'Name',
         property: 'name',
       },
+      {
+        name: 'Description',
+        property: 'description',
+      },
+      {
+        name: 'Local Cluster',
+        property: 'localClusterName',
+      },
+      {
+        name: 'Remote Cluster',
+        property: 'remoteClusterName',
+      },
+      {
+        name: 'Last Modified',
+        property: 'mostRecentChangeDate',
+      },
     ],
   };
   public isLoading = false;
-  public profiles: ActifioProfileDto[] = [];
+  public profiles: ProfileView[] = [];
 
-  constructor(private agmProfileService: V1AgmProfilesService) {}
+  constructor(private agmProfileService: V1AgmProfilesService, private datePipe: DatePipe) {}
 
   ngOnInit(): void {
     this.loadProfiles();
@@ -26,9 +47,24 @@ export class ProfileListComponent implements OnInit {
 
   public loadProfiles(): void {
     this.isLoading = true;
-    this.agmProfileService.v1AgmProfilesGet({}).subscribe(data => {
-      this.profiles = data;
+    this.agmProfileService.v1AgmProfilesGet({ limit: 100, offset: 0 }).subscribe(profiles => {
+      this.profiles = profiles.map(profile => {
+        return {
+          ...profile,
+          description: profile.description || '--',
+          remoteClusterName: profile.remoteClusterName || '--',
+          mostRecentChangeDate: this.getMostRecentChange(profile.lastModifiedDate, profile.createdDate),
+        };
+      });
       this.isLoading = false;
     });
+  }
+
+  private getMostRecentChange(modifiedDate: string, createdDate: string): string {
+    const transformDate = date => (date ? this.datePipe.transform(date, 'M/d/yy, h:mm:ss a') : '--');
+    if (!modifiedDate) {
+      return transformDate(createdDate);
+    }
+    return transformDate(modifiedDate);
   }
 }
