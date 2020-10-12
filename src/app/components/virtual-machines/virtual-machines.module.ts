42c52f956a8c3223104bdda990633a58933d7769
Cody Cahoon <cody.cahoon@levvel.io>
2020-10-12 15:12:00 -0400

    fix: Fixes location of view-field component path (#169)

--- src/app/components/virtual-machines/virtual-machines.module.ts
+++ src/app/components/virtual-machines/virtual-machines.module.ts
@@ -7,7 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SelectVCenterComponent } from './components/vm-discovery-modal/select-vcenter/select-vcenter.component';
 import { SelectVirtualMachinesComponent } from './components/vm-discovery-modal/select-virtual-machines/select-virtual-machines.component';
 import { TableModule } from 'src/app/common/table/table.module';
-import { ViewFieldModule } from 'src/app/common/value-card/view-field.module';
+import { ViewFieldModule } from 'src/app/common/view-field/view-field.module';
 import { VmDiscoveryModalComponent } from './components/vm-discovery-modal/vm-discovery-modal.component';
 import { VmListComponent } from './components/vm-list/vm-list.component';
 import { VmViewComponent } from './components/vm-view/vm-view.component';
