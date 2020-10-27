c4a0534e1d832f3999015a6f10f2d9fe6b35789b
Andrew Clark <apclarkdev@gmail.com>
2020-10-27 17:31:14 -0400

    chore: change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -16,19 +16,34 @@ import { OidcClientSettings } from 'oidc-client';
 //     userinfo_endpoint: 'https://10.151.14.54/cfs/oauth/draasui/userinfo',
 //   },
 // };
+// const openId: OidcClientSettings = {
+//   authority: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui',
+//   client_id: 'XANzoGUU0r1fzyRI24qdd1',
+//   client_secret: 'l4r9C895ssY9bAHU1NVj9vdVO0qMJMrhCVnMNi5RhYj0',
+//   redirect_uri: 'https://dcs-int.draas.cdsvdc.lcl/callback',
+//   response_type: 'token',
+//   scope: 'openid profile',
+//   loadUserInfo: true,
+//   metadata: {
+//     issuer: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui',
+//     authorization_endpoint: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui/authorize',
+//     token_endpoint: 'https://dcs-int.draas.cdsvdc.lcl/cfs/oauth/draasui/tokenservice',
+//     userinfo_endpoint: 'https://dcs-int.draas.cdsvdc.lcl/cfs/oauth/draasui/userinfo',
+//   },
+// };
 const openId: OidcClientSettings = {
   authority: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui',
-  client_id: 'XANzoGUU0r1fzyRI24qdd1',
-  client_secret: 'l4r9C895ssY9bAHU1NVj9vdVO0qMJMrhCVnMNi5RhYj0',
-  redirect_uri: 'https://dcs-int.draas.cdsvdc.lcl/callback',
+  client_id: 'u9jH9wH5gGNtI44uyl2g2Y',
+  client_secret: 'WoQr5zyY2OcuFfM4EslQF8R1WplrtkvDo1MsS2e9ezX1',
+  redirect_uri: 'https://dcs-dev.draas.cdsvdc.lcl/callback',
   response_type: 'token',
   scope: 'openid profile',
   loadUserInfo: true,
   metadata: {
     issuer: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui',
     authorization_endpoint: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui/authorize',
-    token_endpoint: 'https://dcs-int.draas.cdsvdc.lcl/cfs/oauth/draasui/tokenservice',
-    userinfo_endpoint: 'https://dcs-int.draas.cdsvdc.lcl/cfs/oauth/draasui/userinfo',
+    token_endpoint: 'https://dcs-dev.draas.cdsvdc.lcl/cfs/oauth/draasui/tokenservice',
+    userinfo_endpoint: 'https://dcs-dev.draas.cdsvdc.lcl/cfs/oauth/draasui/userinfo',
   },
 };
 
