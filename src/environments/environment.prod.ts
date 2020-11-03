6dd9f2419749ceedb486126e7a416811901267fa
Andrew Clark <apclarkdev@gmail.com>
2020-11-03 13:50:09 -0500

    chore: reset env var for test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,37 +1,36 @@
 import { OidcClientSettings } from 'oidc-client';
 
 // Needs to be updated but necessary for build
-// const openId: OidcClientSettings = {
-//   authority: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui',
-//   client_id: '6710TqF8fKfbx7R4VXowrl',
-//   client_secret: 'AZhsc851y3Nd2hNr6oPk04xMsmnpuDBuMv5IfJJ1eKz0',
-//   redirect_uri: 'https://10.151.14.54/callback',
-//   response_type: 'token',
-//   scope: 'openid profile',
-//   loadUserInfo: true,
-//   metadata: {
-//     issuer: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui',
-//     authorization_endpoint: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui/authorize',
-//     token_endpoint: 'https://10.151.14.54/cfs/oauth/draasui/tokenservice',
-//     userinfo_endpoint: 'https://10.151.14.54/cfs/oauth/draasui/userinfo',
-//   },
-// };
-
 const openId: OidcClientSettings = {
   authority: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui',
-  client_id: 'XANzoGUU0r1fzyRI24qdd1',
-  client_secret: 'l4r9C895ssY9bAHU1NVj9vdVO0qMJMrhCVnMNi5RhYj0',
-  redirect_uri: 'https://dcs-int.draas.cdsvdc.lcl/callback',
+  client_id: '6710TqF8fKfbx7R4VXowrl',
+  client_secret: 'AZhsc851y3Nd2hNr6oPk04xMsmnpuDBuMv5IfJJ1eKz0',
+  redirect_uri: 'https://10.151.14.54/callback',
   response_type: 'token',
   scope: 'openid profile',
   loadUserInfo: true,
   metadata: {
     issuer: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui',
     authorization_endpoint: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui/authorize',
-    token_endpoint: 'https://dcs-int.draas.cdsvdc.lcl/cfs/oauth/draasui/tokenservice',
-    userinfo_endpoint: 'https://dcs-int.draas.cdsvdc.lcl/cfs/oauth/draasui/userinfo',
+    token_endpoint: 'https://10.151.14.54/cfs/oauth/draasui/tokenservice',
+    userinfo_endpoint: 'https://10.151.14.54/cfs/oauth/draasui/userinfo',
   },
 };
+// const openId: OidcClientSettings = {
+//   authority: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui',
+//   client_id: 'XANzoGUU0r1fzyRI24qdd1',
+//   client_secret: 'l4r9C895ssY9bAHU1NVj9vdVO0qMJMrhCVnMNi5RhYj0',
+//   redirect_uri: 'https://dcs-int.draas.cdsvdc.lcl/callback',
+//   response_type: 'token',
+//   scope: 'openid profile',
+//   loadUserInfo: true,
+//   metadata: {
+//     issuer: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui',
+//     authorization_endpoint: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui/authorize',
+//     token_endpoint: 'https://dcs-int.draas.cdsvdc.lcl/cfs/oauth/draasui/tokenservice',
+//     userinfo_endpoint: 'https://dcs-int.draas.cdsvdc.lcl/cfs/oauth/draasui/userinfo',
+//   },
+// };
 
 // const openId: OidcClientSettings = {
 //   authority: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui',
