70b95a45431ace56709da8fd39f4bfa3f8fd69b6
Andrew Clark <apclarkdev@gmail.com>
2020-11-13 16:12:01 -0500

    chore: update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -18,7 +18,7 @@ const openId: OidcClientSettings = {
 
 export const environment = {
   production: false,
-  userClaims: true,
+  userClaims: false,
   apiBase: 'http://localhost:3000/api',
   wikiBase: 'http://wiki.draas.cdsvdc.lcl/index.php/UI',
   openId,
