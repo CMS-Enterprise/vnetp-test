ceb33bbb6d670d0fc723976e41c401bbc6c9bf9b
Andrew Clark <apclarkdev@gmail.com>
2020-11-18 18:48:15 -0500

    chore: fix spec

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   providedIn: 'root',
 })
 export class AuthService {
-  private manager = new UserManager(environment.environment);
+  private manager = new UserManager(environment.environment.oidc);
   private user: BehaviorSubject<User> = new BehaviorSubject<User>(null);
   public isLoggedInBool = false;
   public currentUser: Observable<User> = this.user.asObservable();
@@ -70,7 +70,7 @@ export class AuthService {
   logout(route: string): void {
     this.user.next(null);
     this.currentUser = this.user.asObservable();
-    sessionStorage.setItem(`oidc.user:${environment.environment.authority}:${environment.environment.client_id}`, null);
+    sessionStorage.setItem(`oidc.user:${environment.environment.oidc.authority}:${environment.environment.oidc.client_id}`, null);
     this.router.navigate([`/${route}`], {
       queryParamsHandling: 'merge',
     });
