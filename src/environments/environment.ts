ba12ed1885c102d982be39a4d31f45aa2f3fdf18
Andrew Clark <apclarkdev@gmail.com>
2020-11-18 19:38:47 -0500

    chore: remove unused props

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,32 +1,12 @@
-import { OidcClientSettings } from 'oidc-client';
 import { DynamicEnvironment } from './dynamic-environment';
 
-const openId: OidcClientSettings = {
-  authority: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui',
-  client_id: 'lS12GPO6lWGJ8e7Ocjbo0z',
-  client_secret: 'r0Tjc0OZ6Oem4Biw9ZGulD0mY3xzzBD9Q6wuJ4jIgMft',
-  redirect_uri: 'http://localhost:4200/callback',
-  response_type: 'token',
-  scope: 'openid profile',
-  loadUserInfo: true,
-  metadata: {
-    issuer: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui',
-    authorization_endpoint: 'https://cfs01-dc01.draas.cdsvdc.lcl/cfs/oauth/draasui/authorize',
-    token_endpoint: 'http://localhost:3000/cfs/oauth/draasui/tokenservice',
-    userinfo_endpoint: 'http://localhost:3000/cfs/oauth/draasui/userinfo',
-  },
-};
 class Environment extends DynamicEnvironment {
   public production: boolean;
-  public userClaims: boolean;
-  public openId: OidcClientSettings;
   public apiBase: string;
   public wikiBase: string;
   constructor() {
     super();
     this.production = false;
-    this.userClaims = true;
-    this.openId = openId;
     this.apiBase = 'http://localhost:3000/api';
     this.wikiBase = 'http://wiki.draas.cdsvdc.lcl/index.php/UI';
   }
