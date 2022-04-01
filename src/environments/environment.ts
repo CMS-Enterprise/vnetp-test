14d886af7fd2b86dbef81939d32f5860ed4cc6b1
Patrick McCardle <pmccardle@ineteng.com>
2022-04-01 15:57:15 -0400

    initial commit prepping for PR merge to main branch (#319)
    * initial commit prepping for PR merge to main branch
    
    * additional changes to prep for multi-site functionality
    
    Co-authored-by: delps@ineteng.com <delps@ineteng.com>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,14 @@ class Environment extends DynamicEnvironment {
   public production: boolean;
   public apiBase: string;
   public wikiBase: string;
+  public dcsLocations;
   constructor() {
     super();
     this.production = false;
+    this.dcsLocations = [
+      { name: 'East', url: 'http://localhost-east:4200' },
+      { name: 'West', url: 'http://localhost-west:4200' },
+    ];
     this.apiBase = 'http://localhost:3000/api';
     this.wikiBase = 'http://wiki.draas.cdsvdc.lcl/index.php/UI';
   }
