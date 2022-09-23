558477cac9e3b9d44069f42b415f3e32ba275128
Patrick McCardle <pmccardle@ineteng.com>
2022-09-23 10:08:18 -0400

    begin to break up self service modal into multi-step modal

--- src/app/components/self-service/self-service-modal/self-service-modal.component.ts
+++ src/app/components/self-service/self-service-modal/self-service-modal.component.ts
@@ -10,14 +10,16 @@ import { DatacenterContextService } from 'src/app/services/datacenter-context.se
   templateUrl: './self-service-modal.component.html',
 })
 export class SelfServiceModalComponent implements OnInit, OnDestroy {
-  form: FormGroup;
+  initialForm: FormGroup;
+  continuedForm: FormGroup;
   submitted: boolean;
   tiers: Tier[];
   selectedTier: Tier;
   datacenterId;
   tiersFromConfig = [];
   selectedTiers;
-  nameSpaces;
+  tiersSaved: boolean;
+  storedInterfaces = [];
 
   private currentDatacenterSubscription: Subscription;
 
@@ -30,11 +32,15 @@ export class SelfServiceModalComponent implements OnInit, OnDestroy {
   ) {}
 
   get f() {
-    return this.form.controls;
+    return this.initialForm.controls;
+  }
+
+  get cf() {
+    return this.continuedForm.controls;
   }
 
   private buildForm() {
-    this.form = this.formBuilder.group({
+    this.initialForm = this.formBuilder.group({
       deviceType: ['', Validators.required],
       DCSTierSelect: ['', Validators.required],
       deviceConfig: ['', Validators.required],
@@ -43,16 +49,38 @@ export class SelfServiceModalComponent implements OnInit, OnDestroy {
     });
   }
 
-  public saveNameSpaces() {
-    console.log('this.form', this.form);
-    console.log('this.nameSpaces', this.nameSpaces);
+  public saveTiers() {
+    console.log('this.form', this.initialForm);
+    this.tiersSaved = true;
+    this.initialForm.controls.selectedTiersFromConfig.disable();
+    this.continuedForm = this.formBuilder.group({
+      selectedTiers: [this.initialForm.controls.selectedTiersFromConfig.value],
+    });
   }
 
+  public saveNameSpaces() {
+    const grouped = document.getElementsByClassName('custom');
+    const groupedArray = [];
+    for (let i = 0; i < grouped.length; i++) {
+      groupedArray.push(grouped[i]);
+    }
+    const mappedObjectsArray = [];
+    groupedArray.map(group => {
+      const populateThisObject = { tierName: '', nameSpace: '' };
+      populateThisObject.tierName = group.textContent;
+      populateThisObject.nameSpace = group.lastChild.value;
+      mappedObjectsArray.push(populateThisObject);
+    });
+    console.log('mappedObjectsArray', mappedObjectsArray);
+  }
+
+  public extractInterfaceMatrix() {}
+
   public addFormControlToListItem() {
-    console.log('this.form', this.form);
+    console.log('this.form', this.initialForm);
     //.forEach((moveMaker ) => this.moveMakerForm .addControl(moveMaker.id, new FormControl('', Validators.required)));
-    const selectedTiersFromConfig: [] = this.form.controls.selectedTiersFromConfig.value
-      ? this.form.controls.selectedTiersFromConfig.value
+    const selectedTiersFromConfig: [] = this.initialForm.controls.selectedTiersFromConfig.value
+      ? this.initialForm.controls.selectedTiersFromConfig.value
       : null;
     if (selectedTiersFromConfig === null) {
       return;
@@ -60,9 +88,9 @@ export class SelfServiceModalComponent implements OnInit, OnDestroy {
     selectedTiersFromConfig.forEach(selectedTierFromConfig => {
       let i = '1';
       console.log('selectedTierFromConfig', selectedTierFromConfig);
-      return this.form.addControl(selectedTierFromConfig, new FormControl('', Validators.required));
+      return this.initialForm.addControl(selectedTierFromConfig, new FormControl('', Validators.required));
     });
-    console.log('this.form', this.form);
+    console.log('this.form', this.initialForm);
   }
 
   public getTiers(): void {
@@ -77,14 +105,14 @@ export class SelfServiceModalComponent implements OnInit, OnDestroy {
   }
 
   public deviceConfigFileChange(event) {
-    this.form.controls.deviceType.disable();
-    this.form.controls.DCSTierSelect.disable();
+    this.initialForm.controls.deviceType.disable();
+    this.initialForm.controls.DCSTierSelect.disable();
     const reader = new FileReader();
     const file = event.target.files[0];
     reader.readAsText(file);
     reader.onload = () => {
       const readableText = reader.result.toString();
-      const deviceType = this.form.controls.deviceType.value;
+      const deviceType = this.initialForm.controls.deviceType.value;
       if (deviceType === 'PA') {
         const parser = new DOMParser();
         const parsed = parser.parseFromString(readableText, 'text/xml');
@@ -110,23 +138,88 @@ export class SelfServiceModalComponent implements OnInit, OnDestroy {
 
       // use regex to search for hostname for ASA configs
       if (deviceType === 'ASA') {
-        const regex = /hostname(.*)/g;
-        const hostnames = readableText.match(regex);
-        const hostnameValues = [];
-        hostnames.map(host => {
-          if (!host.includes(' ')) {
-            const index = hostnames.indexOf(host);
-            hostnames.splice(index, 1);
+        const splitFileByLine = readableText.split('\n');
+        const hostnameRegex = /hostname(.*)/g;
+        const nameifRegex = /nameif(.*)/g;
+        const hostnames = readableText.match(hostnameRegex);
+        const interfaces = readableText.match(nameifRegex);
+        const hostnameIndexes = [];
+        hostnames.map(hostname => {
+          const hostnameIndex = splitFileByLine.indexOf(hostname + '\r');
+          if (hostnameIndex !== -1) {
+            hostnameIndexes.push({ hostname: hostname, hostnameIndex: hostnameIndex });
           }
         });
-        hostnames.map(host => {
-          const hostnameVal = host.split(' ')[1];
-          hostnameValues.push(hostnameVal);
+
+        // issue :
+        // if there are duplicate values in the interfaces array
+        // we must find a way to get each unique index of the interfaces
+        // indexOf only returns the first matching instance found and ignores others
+        // we must run the first instance of any duplicate entries to get their index
+        // so we can use that index as an offset to get the next matching instance
+
+        const uniqueIndexes = [...new Set(interfaces)];
+
+        uniqueIndexes.map(int => {
+          this.recursivelyGetIndexes(int, splitFileByLine);
         });
-        // this.form.controls.extractedTiersFromConfig.setValue(hostnameValues);
-        this.tiersFromConfig = hostnameValues;
+        hostnameIndexes.map(host => {
+          host.hostname = host.hostname.split(' ')[1];
+        });
+        this.tiersFromConfig = hostnameIndexes.map(hostnameIndex => {
+          return hostnameIndex.hostname;
+        });
+        const mappedEverything = this.interfaceMatrixHelper(hostnameIndexes, this.storedInterfaces, splitFileByLine.length);
+        console.log('mappedEverything', mappedEverything);
       }
     };
+  }
+
+  //psuedo :
+  // take the value, find the index in the string
+  // if there are multiple entries with the same value
+  // pass the same args to the function this time providing an offset
+  // the offset is the index found in the initial function run
+  private recursivelyGetIndexes(val, file, offset?) {
+    if (offset) {
+      const index = file.indexOf(' ' + val + '\r', offset + 1);
+      if (index !== -1) {
+        this.storedInterfaces.push({ interface: val, index: index });
+        this.recursivelyGetIndexes(val, file, index);
+      } else {
+        return;
+      }
+    } else {
+      const index = file.indexOf(' ' + val + '\r');
+      if (index !== -1) {
+        this.storedInterfaces.push({ interface: val, index: index });
+        this.recursivelyGetIndexes(val, file, index);
+      } else {
+        return;
+      }
+    }
+  }
+
+  private interfaceMatrixHelper(hostnamesWithIndex, interfacesWithIndex, eof) {
+    for (let i = 0; i < hostnamesWithIndex.length; i++) {
+      const range = { min: '', max: '' };
+      range.min = hostnamesWithIndex[i].hostnameIndex;
+      if (hostnamesWithIndex[i + 1]) {
+        range.max = hostnamesWithIndex[i + 1].hostnameIndex;
+      } else {
+        range.max = eof;
+      }
+      hostnamesWithIndex[i].range = range;
+    }
+    hostnamesWithIndex.map(host => {
+      host.interfaces = [];
+      interfacesWithIndex.map(int => {
+        if (int.index > host.range.min && int.index < host.range.max) {
+          host.interfaces.push(int);
+        }
+      });
+    });
+    return hostnamesWithIndex;
   }
 
   public intervrfSubnetsFileChange(event) {
@@ -142,9 +235,12 @@ export class SelfServiceModalComponent implements OnInit, OnDestroy {
   public reset() {
     this.selectedTier = null;
     this.tiersFromConfig = null;
-    this.form.reset();
-    this.form.enable();
-    console.log('this.form', this.form);
+    this.tiersSaved = false;
+    this.initialForm.reset();
+    this.initialForm.enable();
+    this.continuedForm.reset();
+    this.continuedForm.enable();
+    console.log('this.form', this.initialForm);
   }
 
   public save() {}
