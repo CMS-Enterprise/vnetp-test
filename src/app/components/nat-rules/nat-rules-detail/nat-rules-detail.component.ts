241232ece424d96c50aadc0e7efe648468489703
Daniel Elps <delps@ineteng.com>
2023-01-04 14:22:08 -0500

    Add deletedAt||isnull filter for obj/objgrp API calls from FW and NAT rule modals.

--- src/app/components/nat-rules/nat-rules-detail/nat-rules-detail.component.ts
+++ src/app/components/nat-rules/nat-rules-detail/nat-rules-detail.component.ts
@@ -186,21 +186,21 @@ export class NatRulesDetailComponent implements OnInit, OnDestroy {
   getObjects(): void {
     const tierRequest = this.tierService.getOneTier({ id: this.TierId });
     const networkObjectRequest = this.networkObjectService.getManyNetworkObject({
-      filter: [`tierId||eq||${this.TierId}`],
+      filter: [`tierId||eq||${this.TierId}`, `deletedAt||isnull`],
       fields: ['id,name'],
       sort: ['updatedAt,ASC'],
       page: 1,
       limit: 50000,
     });
     const networkObjectGroupRequest = this.networkObjectGroupService.getManyNetworkObjectGroup({
-      filter: [`tierId||eq||${this.TierId}`],
+      filter: [`tierId||eq||${this.TierId}`, `deletedAt||isnull`],
       fields: ['id,name'],
       sort: ['updatedAt,ASC'],
       page: 1,
       limit: 50000,
     });
     const serviceObjectRequest = this.serviceObjectService.getManyServiceObject({
-      filter: [`tierId||eq||${this.TierId}`],
+      filter: [`tierId||eq||${this.TierId}`, `deletedAt||isnull`],
       fields: ['id,name'],
       sort: ['updatedAt,ASC'],
       page: 1,
