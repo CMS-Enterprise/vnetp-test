9f8e274f8689f36efba42f481d6201506c54028a
Patrick McCardle <pmccardle@ineteng.com>
2023-03-01 15:25:44 -0500

    updated openapispec

--- client/model/datacenter.ts
+++ client/model/datacenter.ts
@@ -15,6 +15,7 @@ import { Tier } from './tier';
 import { TierGroup } from './tierGroup';
 import { PhysicalServer } from './physicalServer';
 import { PriorityGroup } from './priorityGroup';
+import { SelfService } from './selfService';
 import { Job } from './job';
 import { AuditLog } from './auditLog';
 
@@ -35,6 +36,6 @@ export interface Datacenter {
     readonly tierGroups?: Array<TierGroup>;
     readonly jobs?: Array<Job>;
     readonly auditLogs?: Array<AuditLog>;
-    readonly selfServices?: Array<string>;
+    readonly selfServices?: SelfService;
 }
 
