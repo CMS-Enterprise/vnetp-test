48d396589990a58aaa9fcda18bba5aed06456337
Daniel Elps <delps@ineteng.com>
2023-04-18 15:16:55 -0400

    Remove legacy items from dashboard.

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { V1DatacentersService, V1TiersService, V1VmwareVirtualMachinesService, V1LoadBalancerVirtualServersService, UserDto } from 'client';
+import { V1DatacentersService, V1TiersService, V1LoadBalancerVirtualServersService, UserDto } from 'client';
 import { DashboardHelpText } from 'src/app/helptext/help-text-networking';
 import { PieChartData } from 'src/app/common/d3-pie-chart/d3-pie-chart.component';
 import { AuthService } from '../../services/auth.service';
