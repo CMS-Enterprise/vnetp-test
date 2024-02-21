92d06fcf500aae1b7d1cfea317b4929a1feb383c
Andrew Crapo <acrapo@D401CVDJN6.local>
2024-02-21 11:20:43 -0500

    fwr fully implemented

--- src/app/components/firewall-rules/firewall-rule-packet-tracer/firewall-rule-packet-tracer.component.ts
+++ src/app/components/firewall-rules/firewall-rule-packet-tracer/firewall-rule-packet-tracer.component.ts
@@ -1,11 +1,9 @@
 /* tslint:disable */
 
-import { Component, ElementRef, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgxSmartModalService } from 'ngx-smart-modal';
-import { Subscription } from 'rxjs';
 import { IpAddressAnyValidator, ValidatePortRange } from 'src/app/validators/network-form-validators';
-import { FirewallRuleDestinationAddressTypeEnum, FirewallRuleSourceAddressTypeEnum } from '../../../../../client';
 import { Netmask } from 'netmask';
 
 @Component({
@@ -17,18 +15,13 @@ export class FirewallRulePacketTracerComponent implements OnInit {
   @Input() objects;
   form: FormGroup;
   submitted: boolean;
-  modalBody;
-  modalTitle;
 
   rulesHit = [];
-  partialMatches = [];
-  showPartials = false;
-  doneSearching = false;
-  protocolSubscription: Subscription;
-
   filteredRules = [];
+
   currentPage = 1;
   pageSize = 10;
+
   filterExact = false;
   filterPartial = false;
 
@@ -43,6 +36,15 @@ export class FirewallRulePacketTracerComponent implements OnInit {
 
   toggleDropdown(): void {
     this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const clickedElement = event.target as HTMLElement; // Cast to HTMLElement
+
+    if (!clickedElement.closest('.dropdown')) {
+      this.dropdownOpen = false;
+    }
   }
 
   isExactMatch(rule): boolean {
@@ -65,73 +67,14 @@ export class FirewallRulePacketTracerComponent implements OnInit {
     this.currentPage = 1;
   }
 
+  resetFilter(): void {
+    this.filterExact = false;
+    this.filterPartial = false;
+  }
+
   get paginatedRules() {
     const startIndex = (this.currentPage - 1) * this.pageSize;
     return this.filteredRules.slice(startIndex, startIndex + this.pageSize);
-  }
-
-  // takes an ipAddress to search for and another IP Subnet and determines if the ip to search
-  // falls within range
-  calculateSubnet(ipToSearch, ipAddress?) {
-    const split = ipAddress.split('/');
-    // determine if there is a CIDR
-    const [ip, cidr] = split;
-
-    // converts all IP addresses into decimal format
-    const ipNumber = this.dot2num(ip);
-    const ipToSearchNum = this.dot2num(ipToSearch);
-
-    // sends all parameters to function that determins the range of IPs
-
-    // if we ever want to display all subnets
-    // const subnets = [];
-    // for (let i = 32; i >= 0; i--) {
-    //   subnets.push(this.num2dot(2 ** 32 - 2 ** i));
-    // }
-    // console.log('subnets', subnets)
-
-    return this.ipToRange(ipToSearchNum, ipNumber, cidr);
-  }
-
-  cidrSize(cidrSlash): number {
-    return Math.pow(2, 32 - cidrSlash);
-  }
-
-  // caluculates whether an IP falls within a subnet or not
-  ipToRange(ipToSearchNum, ip, cidr) {
-    const size = this.cidrSize(cidr);
-    const startIpNum = ip - (ip % size);
-    const endIpNum = startIpNum + size - 1;
-    let inRange = false;
-
-    // if ipToCheck is inbetween our startIp and endIp
-    // we know that ipToCheck falls within range
-    if (startIpNum <= ipToSearchNum && endIpNum >= ipToSearchNum) {
-      inRange = true;
-    }
-
-    // converts back to IP addresses
-    const ipToCheckStr = this.num2dot(ipToSearchNum);
-    const startIpStr = this.num2dot(startIpNum);
-    const endIpStr = this.num2dot(endIpNum);
-    return {
-      ipToCheckStr,
-      cidr,
-      size,
-      startIpStr,
-      endIpStr,
-      inRange,
-    };
-  }
-
-  // converts decimal IPs back to octect format
-  num2dot(num) {
-    let d: any = num % 256;
-    for (let i = 3; i > 0; i--) {
-      num = Math.floor(num / 256);
-      d = (num % 256) + '.' + d;
-    }
-    return d;
   }
 
   // converts octect IPs to decimals
@@ -140,28 +83,6 @@ export class FirewallRulePacketTracerComponent implements OnInit {
     return ((+d[0] * 256 + +d[1]) * 256 + +d[2]) * 256 + +d[3];
   }
 
-  // // TO DO : IPv6
-  // convertIpv6(ipv6): void {
-  //   // const ipv6Subnet = '2001:db8:0:0:8d3::/64';
-  //   // simulate your address.binaryZeroPad(); method
-  //   var parts = [];
-  //   ipv6.split(':').forEach(function(it) {
-  //     var bin = parseInt(it, 16).toString(2);
-  //     while (bin.length < 16) {
-  //       bin = '0' + bin;
-  //     }
-  //     parts.push(bin);
-  //   });
-  //   var bin = parts.join('');
-
-  //   // Use BigInteger library
-  //   // var dec = BigInt(bin).toString()
-  //   // var dec2 = parseInt(dec, 2)
-  //   // var dec3 = BigInt(dec2)
-  //   console.log(bin);
-  // }
-
-  // TO DO : IPv6 Searches
   search() {
     this.rulesHit = [];
     this.submitted = true;
@@ -187,6 +108,8 @@ export class FirewallRulePacketTracerComponent implements OnInit {
 
       this.rulesHit.push({ checkList, name: rule.name });
     });
+    this.resetFilter();
+    this.applyFilter();
   }
 
   handleInRange(rule, location: 'source' | 'destination', control: AbstractControl) {
@@ -346,9 +269,8 @@ export class FirewallRulePacketTracerComponent implements OnInit {
   reset(): void {
     this.submitted = false;
     this.rulesHit = [];
-    this.partialMatches = [];
-    this.showPartials = false;
     this.form.reset();
+    this.resetFilter();
     this.ngx.resetModalData('firewallRulePacketTracer');
     this.buildForm();
   }
