03c8a411bae03d5c724b7662fea5f67d1fa7492a
Patrick McCardle <pmccardle@ineteng.com>
2024-03-28 14:09:12 -0400

    added missing type

--- src/app/common/audit-log/audit-log.component.ts
+++ src/app/common/audit-log/audit-log.component.ts
@@ -84,7 +84,7 @@ export class AuditLogComponent implements OnInit {
   routeProfiles: RouteProfile[];
   l3Outs: L3Out[];
   appProfiles: ApplicationProfile[];
-  bridgeDomains: BridgeDomain;
+  bridgeDomains: BridgeDomain[];
   endpointGroups: EndpointGroup[];
   providedContracts: Contract[];
   consumedContracts: Contract[];
