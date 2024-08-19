cad22dde5d11c4237e80145636a191d8e4cac65f
Daniel Elps <delps@ineteng.com>
2024-08-19 11:34:21 -0400

    fix line length

--- src/app/components/f5-config/partition-details/partition-details.component.spec.ts
+++ src/app/components/f5-config/partition-details/partition-details.component.spec.ts
@@ -64,7 +64,8 @@ describe('PartitionDetailsComponent', () => {
   describe('ngOnInit', () => {
     beforeEach(() => {
       data = {
-        partitions: [{ name: 'partition1', virtualServers: [{ name: 'virtualServer1' }] } as F5RuntimePartitionInfo] as unknown as string[], // Temporarily casting to bypass type checks
+        partitions: [{ name: 'partition1', virtualServers: [{ name: 'virtualServer1' }]
+      } as F5RuntimePartitionInfo] as unknown as string[], // Temporarily casting to bypass type checks
       } as Partial<F5Runtime>;
       fixture.detectChanges();
     });
