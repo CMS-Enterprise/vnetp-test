0254b834a0a86b3cff0d17728f296d7d7f2e81bf
Andrew Crapo <acrapo@D401CVDJN6.local>
2024-10-04 17:36:14 -0400

    fixed lodash import

--- src/app/services/tier-context.service.ts
+++ src/app/services/tier-context.service.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { Tier, V1DatacentersService } from 'client';
 import { DatacenterContextService } from './datacenter-context.service';
-import _ from 'lodash';
+import * as _ from 'lodash';
 
 /** Service to store and expose the Current Tier Context. */
 @Injectable({
