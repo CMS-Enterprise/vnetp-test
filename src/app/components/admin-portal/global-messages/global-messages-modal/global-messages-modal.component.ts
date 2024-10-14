e2b7e52025497f00f8accc891b260e4046d1063d
Patrick McCardle <pmccardle@ineteng.com>
2024-10-14 14:20:18 -0400

    begin adding global message types for dashboard display

--- src/app/components/admin-portal/global-messages/global-messages-modal/global-messages-modal.component.ts
+++ src/app/components/admin-portal/global-messages/global-messages-modal/global-messages-modal.component.ts
@@ -48,6 +48,7 @@ export class GlobalMessagesModalComponent implements OnInit {
 
   private buildForm(): void {
     this.form = this.formBuilder.group({
+      messageType: ['', Validators.required],
       description: ['', Validators.compose([Validators.maxLength(100), Validators.required])],
     });
   }
